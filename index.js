15e62bee53a6d1bddeb12e78d21199e990bd0768
Kinan Dak Albab <kinan.bab.14@gmail.com>
2017-06-01 12:28:43 -0400

    use skeleton of the socket.io chat example

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.listen(3000, function(){
+  console.log('listening on *:3000');
+});
