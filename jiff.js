942bc4b1794468bf67f793ff8545a388876b86da
Rawane Issa <rawaneissa@gmail.com>
2017-06-02 14:27:37 -0400

    implemented addition

--- jiff.js
+++ jiff.js
@@ -5,8 +5,8 @@ var mod = Math.pow(2, 31) - 1;
  * Share given secret to the participating parties.
  *   jiff:    the jiff instance.
  *   secret:  the secret to share.
- *   return:  a (JQuery) promise to a map (of size equal to 
- *            the number of parties) where the key is the party 
+ *   return:  a (JQuery) promise to a map (of size equal to
+ *            the number of parties) where the key is the party
  *            id (from 1 to n) and the value is the share object
  *            that wrapped the value sent from that party.
  *
@@ -15,33 +15,33 @@ function jiff_share(jiff, secret) {
   var count = jiff.party_count;
   var sum_mod = 0;
   var shares = {};
-  var share_id = "share" + jiff.sharing_count;  
+  var share_id = "share" + jiff.sharing_count;
   jiff.sharing_count++;
-  
+
   // Compute first n-1 shares (random numbers)
-  for(var i = 1; i < count; i++) {  
+  for(var i = 1; i < count; i++) {
     var share = Math.floor(Math.random() * mod);
     sum_mod = (sum_mod + share) % mod;
     shares[i] = share;
   }
-  
+
   // Compute last share
   var share = (secret - sum_mod) % mod;
   if(share < 0) { share = share + mod; }
   shares[count] = share;
-  
+
   // Setup a deffered for receiving the shares from other parties.
   var deferred = $.Deferred();
   jiff.deferreds[share_id] = deferred;
-    
+
   // Shares have been computed, share them.
   for(var i = 1; i <= count; i++) {
     if(i == jiff.id) { receive_share(jiff, i, shares[i], share_id); continue; }
-    
+
     var msg = { party_id: i, share: shares[i], share_id: share_id };
     jiff.socket.emit('share', JSON.stringify(msg));
   }
-  
+
   // Defer accessing the shares until they are back
   return deferred.promise();
 }
@@ -55,23 +55,23 @@ function jiff_share(jiff, secret) {
 */
 function jiff_open(jiff, share) {
   if(!(share.jiff === jiff)) { throw "share does not belong to given instance"; }
-  
+
   var count = jiff.party_count;
-  var share_id = "open" + jiff.sharing_count;  
+  var share_id = "open" + jiff.sharing_count;
   jiff.sharing_count++;
-    
+
   // Setup a deffered for receiving the shares from other parties.
   var deferred = $.Deferred();
   jiff.deferreds[share_id] = deferred;
-    
+
   // Shares have been computed, share them.
   for(var i = 1; i <= count; i++) {
     if(i == jiff.id) { receive_open(jiff, i, share.value, share_id); continue; }
-    
+
     var msg = { party_id: i, share: share.value, share_id: share_id };
     jiff.socket.emit('open', JSON.stringify(msg));
   }
-  
+
   // Defer accessing the shares until they are back
   return deferred.promise();
 }
@@ -93,19 +93,19 @@ function receive_share(jiff, sender_id, share, share_id) {
 
     // Update share
     jiff.shares[share_id][sender_id] = new secret_share(jiff, share);
-    
+
     // Check if all shares were received
     var shares = jiff.shares[share_id];
     for(var i = 1; i <= jiff.party_count; i++) {
       if(shares[i] == null) return;
     }
-    
+
     // Everything was received, resolve the deferred.
     jiff.deferreds[share_id].resolve(shares);
 }
 
 /*
- * Store the received share of the secret to open, reconstruct 
+ * Store the received share of the secret to open, reconstruct
  * the secret and resolves the corresponding deferred if needed.
  *   jiff:      the jiff instance.
  *   sender_id: the id of the sender.
@@ -121,7 +121,7 @@ function receive_open(jiff, sender_id, share, share_id) {
 
     // Update share
     jiff.shares[share_id][sender_id] = share;
-    
+
     // Check if all shares were received
     var shares = jiff.shares[share_id];
     var sum_mod = 0;
@@ -129,14 +129,14 @@ function receive_open(jiff, sender_id, share, share_id) {
       if(shares[i] == null) return;
       sum_mod = (sum_mod + shares[i]) % mod;
     }
-    
+
     // Everything was received, resolve the deferred.
     jiff.deferreds[share_id].resolve(sum_mod);
 }
 
 /*
- * Create a new share. 
- * A share is a value wrapper with a share object, it has a unique id 
+ * Create a new share.
+ * A share is a value wrapper with a share object, it has a unique id
  * (per computation instance), and a pointer to the instance it belongs to.
  * A share also has methods for performing operations.
  *   jiff:    the jiff instance.
@@ -144,31 +144,35 @@ function receive_open(jiff, sender_id, share, share_id) {
  */
 function secret_share(jiff, value) {
   var self = this;
-  
+
   this.jiff = jiff;
   this.value = value;
-  
+
   this.id = "share"+jiff.shares_count;
   jiff.shares_count++;
-  
+
   this.valueOf = function() {
     return self.value;
   };
-  
+
   this.toString = function() {
     return self.id + ": " + self.value;
   }
-  
+
   // Implement addition
   this.add = function(o) {
-    return self;
+    if (!(o.jiff === self.jiff)){ throw "shares do not belong to the same instance"; }
+    var sum = 0;
+    sum = (o.value + self.value) % mod;
+
+    return new secret_share(self.jiff, sum);
   }
-  
+
   // Implement addition
   this.mult = function(o) {
     return self;
   }
-  
+
   // Implement less than
   this.less = function(o) {
     return self;
@@ -181,55 +185,55 @@ function secret_share(jiff, value) {
  *   port:        server port.
  *   party_count: the number of parties in the computation (> 1).
  *   return:      the jiff instance for the described computation.
- * 
- * The Jiff instance contains the socket, number of parties, functions 
+ *
+ * The Jiff instance contains the socket, number of parties, functions
  * to share and perform operations, as well as synchronization flags.
 */
 function make_jiff(hostname, port, party_count) {
   var jiff = { party_count: party_count, ready: false };
-  
+
   jiff.socket = io(hostname+":"+port);
   jiff.share = function(secret) { return jiff_share(jiff, secret); };
   jiff.open = function(share) { return jiff_open(jiff, share); };
-  
+
   // Store the id when server sends it back
   jiff.socket.on('init', function(msg) {
     jiff.id = parseInt(msg);
     jiff.ready = true;
   });
-  
-  // Store sharing and shares counter which keeps track of the count of 
+
+  // Store sharing and shares counter which keeps track of the count of
   // sharing operations (share and open) and the total number of shares
-  // respectively (used to get a unique id for each share operation and 
+  // respectively (used to get a unique id for each share operation and
   // share object).
   jiff.sharing_count = 0;
   jiff.shares_count = 0;
-  
-  // Store a map from a sharing id (which share operation) to the 
+
+  // Store a map from a sharing id (which share operation) to the
   // corresponding deferred and shares array.
   jiff.deferreds = {};
   jiff.shares = {};
-  
+
   // Setup receiving matching shares
   jiff.socket.on('share', function(msg) {
     json_msg = JSON.parse(msg);
-    
+
     sender_id = json_msg["party_id"];
     share_id = json_msg["share_id"];
     share = json_msg["share"];
-    
+
     receive_share(jiff, sender_id, share, share_id);
   });
-  
+
   jiff.socket.on('open', function(msg) {
     json_msg = JSON.parse(msg);
-    
+
     sender_id = json_msg["party_id"];
     share_id = json_msg["share_id"];
     share = json_msg["share"];
-    
+
     receive_open(jiff, sender_id, share, share_id);
   });
-  
+
   return jiff;
 }
