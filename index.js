b7451cd407c2bce351ef9e9c7df7d4fee9e844f9
Rawane Issa <rawaneissa@gmail.com>
2017-06-21 16:24:08 -0400

    test computation id with encrytion off

--- index.js
+++ index.js
@@ -1,72 +1,92 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var socket_map = {'1':{},'2':{},'3':{}};
-var party_map = {};
-var computation_map = {};
-var client_map = {'1':0, '2':0, '3':0};//total number of parties per computation
-var totalparty_map = {'1':3, '2':3, '3':3};//max number of parties per computation
-var key_map = {};
-var total_parties = 3;
 
-io.on('connection', function(socket){
+var socket_map = {'1':{},'2':{},'3':{}};// map of maps [computation id][party id] = socket id
 
+var party_map = {};// socket.id -> party_id
+var computation_map = {}; // socket.id -> computation_id
+
+var client_map = {'1': 0};//total number of parties per computation
+var totalparty_map = {'1': 3};//max number of parties per computation
+
+var key_map = {};// public key map
+
+io.on('connection', function(socket) {
   console.log('user connected');
 
   // Receive each user's desired computation
-  socket.on('computation_id', function(msg){
-    client_map[msg]++;
+  socket.on('computation_id', function(msg) {
+    var party_id = ++(client_map[msg]);
+
     computation_map[socket.id] = msg;
-    socket_map[msg][client_map[msg]] = socket.id;
-    party_map[socket.id] = client_map[msg];
-    io.to(socket.id).emit('init', party_map[socket.id]);
+    party_map[socket.id] = party_id;
+
+    socket_map[msg][party_id] = socket.id;
+    io.to(socket.id).emit('init', party_id);
   });
 
   // Receive each user's public key
-  socket.on('public_key', function(msg){
+  socket.on('public_key', function(msg) {
     var party_id = party_map[socket.id];
+    var computation_id = computation_map[socket.id];
 
-    key_map[party_id] = msg;
+    // store public key in key_map
+    var tmp = key_map[computation_id];
+    if(tmp == null) tmp = {};
+    tmp[party_id] = msg;
+    key_map[computation_id] = tmp;
 
+    // Check if all public keys for this computation are received
     var full = true;
-    for(var i=1; i <= total_parties; i++){
-
-      if(key_map[i] == null){
+    for(var i = 1; i <= totalparty_map[computation_id]; i++) {
+      if(key_map[computation_id][i] == null) {
         full = false;
         break;
       }
     }
 
-    if(full){
-      io.emit('public_key', JSON.stringify(key_map));
+    // If everyone sent their public key, emit all the keys to everyone
+    if(full) {
+      io.emit('public_key', JSON.stringify(key_map[computation_id]));
     }
   });
 
-
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     console.log('user disconnected');
+    var computation_id = computation_map[socket.id];
+    var party_id = party_map[socket.id];
+
     party_map[socket.id] = null;
-    socket_map[computation_map[socket.id]][party_map[socket.id]] = null;
+    computation_map[socket.id] = null;
+    socket_map[computation_id][party_id] = null;
+    key_map[computation_id][party_id] = null;
   });
 
-  socket.on('share', function(msg){
-    console.log('share from ' + party_map[socket.id] + ' : ' + msg);
+  socket.on('share', function(msg) {
+    var computation_id = computation_map[socket.id];
+    var from_id = party_map[socket.id];
+
+    console.log('share from ' + computation_id + "-" + from_id + ' : ' + msg);
 
     var json_msg = JSON.parse(msg);
-    var index = json_msg["party_id"];
-    json_msg["party_id"] = party_map[socket.id];
+    var to_id = json_msg["party_id"];
+    json_msg["party_id"] = from_id;
 
-    io.to(socket_map[computation_map[socket.id]][index]).emit('share', JSON.stringify(json_msg));
+    io.to(socket_map[computation_id][to_id]).emit('share', JSON.stringify(json_msg));
   });
 
-  socket.on('open', function(msg){
-    console.log('open from ' + party_map[socket.id] + ' : ' + msg);
+  socket.on('open', function(msg) {
+    var computation_id = computation_map[socket.id];
+    var from_id = party_map[socket.id];
+
+    console.log('open from ' + computation_id + "-" + from_id + ' : ' + msg);
 
     var json_msg = JSON.parse(msg);
-    var index = json_msg["party_id"];
-    json_msg["party_id"] = party_map[socket.id];
+    var to_id = json_msg["party_id"];
+    json_msg["party_id"] = from_id;
 
-    io.to(socket_map[computation_map[socket.id]][index]).emit('open', JSON.stringify(json_msg));
+    io.to(socket_map[computation_id][to_id]).emit('open', JSON.stringify(json_msg));
   });
 
 });
