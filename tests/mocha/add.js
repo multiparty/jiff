9dce5c616d6b46c7be99a543b74e3ed96d7787ba
Rawane Issa <rawaneissa@gmail.com>
2017-11-05 21:15:59 -0500

    Remove hardcoded test and randomly generate them.

--- tests/mocha/add.js
+++ tests/mocha/add.js
@@ -1,32 +1,21 @@
 var jiff = require ("../../lib/jiff-client.js");
 
-var tests = [
-    [674, 474, 663], [408, 291, 38], [640, 108, 764],
-    [129, 238, 182], [494, 1005, 325], [163, 929, 554],
-    [541, 61, 818], [269, 153, 405], [152, 172, 37],
-    [600, 345, 993], [10, 871, 712], [640, 409, 487],
-    [674, 129, 689], [1004, 286, 995], [970, 533, 714],
-    [296, 953, 464], [146, 877, 406], [378, 241, 290],
-    [736, 641, 938], [312, 558, 681], [687, 999, 759],
-    [1016, 1011, 831], [1006, 743, 603], [710, 824, 673],
-    [858, 518, 500], [150, 756, 117], [603, 371, 8],
-    [89, 923, 448], [415, 535, 105], [876, 445, 589],
-    [354, 729, 774], [883, 1029, 361], [235, 703, 222],
-    [941, 566, 651], [375, 806, 662], [997, 827, 941],
-    [253, 596, 997], [886, 96, 554], [417, 929, 813],
-    [230, 716, 256], [499, 936, 761], [452, 74, 168],
-    [525, 679, 331], [116, 491, 719], [963, 933, 67],
-    [897, 246, 478], [1019, 502, 245], [154, 78, 717],
-    [711, 497, 717], [983, 112, 38]
-  ];
-
 var jiff_instances = null;
-var parties = tests[0].length;
+var parties = 0;
+var tests = [];
 var has_failed = false;
 
 function run_test(computation_id, callback) {
+  for (var i = 0; i < 50; i++){
+    var num1 = Math.floor(Math.random()*jiff.gZp);
+    var num2 = Math.floor(Math.random()*jiff.gZp);
+    var num3 = Math.floor(Math.random()*jiff.gZp);
+    tests[i] = [num1, num2, num3];
+  }
+
+  parties = tests[0].length;
   computation_id = computation_id + "";
-  
+
   var counter = 0;
   options = { party_count: parties };
   options.onConnect = function() { counter++; if(counter == 3) test(callback); };
@@ -78,7 +67,7 @@ function test_output(index, result) {
     sum += numbers[i];
   }
 
-  sum = jiff.mod(sum, Zp);
+  sum = jiff.mod(sum, jiff.gZp);
   if(sum != result) { // sum is incorrect
     has_failed = true;
     console.log(numbers.join(" + ") + " != " + result);
