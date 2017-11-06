3c0af86d4747002210f6cae528c34b78aa183eb7
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2017-11-06 18:03:54 -0500

    Server instance provide API to attach server side computtion and has matching API to that of client

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var jiff_instance = require('./lib/jiff-server').make_jiff(http);
 // Define a computation with id '1' with a maximum of 3 participants
 jiff_instance.totalparty_map['1'] = 3;
 
+jiff_instance.compute('test-sum', function(computation_instance) {
+  // Perform server side computation
+  console.log("HELLO");
+});
+
 // Server static files
 app.use("/apps", express.static("apps"));
 app.use("/lib", express.static("lib"));
