d1aaa9fe5ce4c1351d7d14a8e0454abe2f6db25b
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2017-11-14 01:47:42 -0500

    Thresholded sharing:
    1. Threshold is specified at share time.
    2. Threshold is propagated with shares during operations, if two shares have different thresholds and are participating in an operation, the resulting share's threshold is the max.
    3. Triplets, numbers, and other mics helpers uses threshold that match the share they operate with.
    4. Open waits for all the shares, this could change if we want, so that it only waits for threshold shares.
    
    Additionally, jiff_share_all_number now supports specifying senders, receivers, and threshold.
    Also, client can now disconnect when the computation is done.

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ jiff_instance.totalparty_map['1'] = 2;
 jiff_instance.compute('test-sum', function(computation_instance) {
   // Perform server side computation
   console.log("HELLO");
-  var shares = computation_instance.share(10, [1,2], [1, 2, "s1"]);
-  shares = computation_instance.share(null, ["s1"], [1, 2]);
+  var shares = computation_instance.share(10, null, [1,2], [1, 2, "s1"]);
+  shares = computation_instance.share(null, 1, ["s1"], [1, 2]);
 
   var r = shares[1].add(shares[2]).add_cst(1);
   r.open(console.log);
--- server-apps/sum_prod.js
+++ server-apps/sum_prod.js
@@ -3,8 +3,8 @@ var jiff = require('../lib/jiff-client');
 
 var options = {party_count: 2};
 options.onConnect = function() {
-  var shares = jiff_instance.share(3, [1, 2], [1, 2, "s1"]);
-  var shares2 = jiff_instance.share(5, ["s1"], [1, 2]);
+  var shares = jiff_instance.share(3, null, [1, 2], [1, 2, "s1"]);
+  var shares2 = jiff_instance.share(5, 1, ["s1"], [1, 2]);
 
   var arr = [];
   for(var i in shares)
@@ -12,9 +12,11 @@ options.onConnect = function() {
 
   var r = arr[0].add(arr[1]);
   r = r.mult(arr[2]);
-  r.open(console.log);
-
-  jiff_instance.receive_open(["s1"]).then(function(result) { console.log("11 is: " + result); });
+  
+  var p1 = r.open(console.log);
+  var p2 = jiff_instance.receive_open(["s1"]).then(function(result) { console.log("11 is: " + result); });
+  
+  Promise.all([p1, p2]).then(jiff_instance.disconnect);
 }
 
 jiff_instance = jiff.make_jiff("http://localhost:3000", 'test-sum', options);
