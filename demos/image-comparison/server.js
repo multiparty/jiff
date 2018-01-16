7d27d1667c81f0496c6dfb6ef5611e0a8923512b
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2018-01-15 19:41:47 -0500

    Clean up goldie to match convention

--- demos/image-comparison/server.js
+++ demos/image-comparison/server.js
@@ -5,12 +5,6 @@ const jiff_instance = require('../../lib/jiff-server').make_jiff(http, {logs:tru
 
 jiff_instance.totalparty_map['1'] = 2;
 
-jiff_instance.compute('1', function(computation_instance){
-  // perform server-side computation
-  console.log('Hello');
-});
-
-
 // Serve static files.
 app.use("/demos", express.static("demos"));
 app.use("/lib", express.static("lib"));
@@ -19,4 +13,4 @@ http.listen(8080, function() {
   console.log('listening on *:8080');
 });
 
-console.log("Direct your browser to *:8080/demos/image-comparison/index.html.");
+console.log("Direct your browser to *:8080/demos/image-comparison/client.html.");
