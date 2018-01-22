934847d6d5acdec1606294bd6ce526464a88988a
lucyq <lucyzqin@gmail.com>
2018-01-22 11:00:06 -0500

    basic file set up

---
+++ demos/concat/client.js
@@ -0,0 +1,35 @@
+
+// Called when the connect button is clicked: connect to the server and intialize the MPC.
+function connect() {
+  // Disable connect button
+  $('#connectBtn').prop('disabled', true);
+
+  // Figure out parameters to intialize the instance.
+  var computation_id = 1;
+  var party_count = 2;
+  
+  // Figure out the hostname of the server from the currently open URL.
+  var hostname = window.location.hostname.trim();
+  var port = window.location.port;
+  if(port == null || port == '') 
+    port = "8080";
+  if(!(hostname.startsWith("http://") || hostname.startsWith("https://")))
+    hostname = "http://" + hostname;
+  if(hostname.endsWith("/"))
+    hostname = hostname.substring(0, hostname.length-1);
+  if(hostname.indexOf(":") > -1)
+    hostanme = hostname.substring(0, hostname.indexOf(":"));
+  hostname = hostname + ":" + port;
+
+  // Create an MPC instance and connect
+  MPCconnect(hostname, computation_id, party_count);
+}
+
+    // Create a JIFF instance and connect to the server.
+function MPCconnect(hostname, computation_id, party_count) {
+  var options = { 'party_count': party_count };
+  options.onError = function(error) { $("#result").append("<p class='error'>"+error+"</p>"); };
+  options.onConnect = function() { $("#concatBtn").attr("disabled", false); $("#result").append("<p>All parties Connected!</p>"); };
+
+  jiff_instance = jiff.make_jiff(hostname, computation_id, options);
+}
