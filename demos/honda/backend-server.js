e7baa867bd06165f8d23b1ffe48ba8d55eb95329
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2018-02-06 00:20:27 -0500

    Begin work on backend server

--- demos/honda/backend-server.js
+++ demos/honda/backend-server.js
@@ -1,37 +1,108 @@
-function run(callback, size) {
-    var jiff_client = require('../../lib/jiff-client');
-    var jiff_instance = jiff_client.make_jiff("http://localhost:8080", '1', { party_id: 1, onConnect: function() { compute(jiff_instance, callback, size) }});
+/*
+ * Backend server:
+ * 1. Establishes a jiff computation/instance with the other frontend servers
+ * 2. Runs a web-server that exposes an API for computing/recomputing all pairs shortest paths locally.
+ * 3. Executes the pre-processing protocol with frontend servers (oblivious shuffle + collision free PRF) on the all pairs shortests paths every time they are computed.
+ * 4. When a frontend server demands: executes retrival protocol (local access by index).
+ */
+var jiff_client = require('../../lib/jiff-client');
+
+var options = {
+  party_id: 1,
+  onConnect: startServer
+};
+var jiff_instance = jiff_client.make_jiff("http://localhost:3000", 'shortest-path-1', options);
+
+function startServer(jiff) {
+  var express = require('express');
+  var app = express();
+
+  // when http://localhost:8080/compute/<input> is called,
+  // server recomputes shortest paths according to what is
+  // defined in the file: ./<input>.json
+  app.get('/recompute/:input', function(req, res)) {
+    console.log("Recomputation requested!");
+
+    var shortest_path_table = compute_shortest_path(req.params.id);
+    mpc_preprocess_table(shortest_path_table);
+
+    res.send("Recomputed! MPC Preprocessing now underway");
+  }
+
+  // Start listening on port 9111
+  app.listen(9111, function() {
+    console.log('backend server up and listening on 9111');
+  });
 }
 
-function compute(jiff_instance, callback, array_size) {
-    console.log("BACK-END START");
-    // Come up with an array of numbers
-    var array = [];
-    for(var i = 1; i <= array_size; i++) array.push(i);
+/* Compute shortest path according to ./<input_file>.json */
+function compute_shortest_path(input_file) {
+  var shortest_path_table = require("./"+input_file+".json");
+  return shortest_path_table;
+}
 
-    // Share the array
-    var senders = [ 1 ]; // Backend server is the only sender.
-    var receivers = [ 2, 3, 4 ]; // Frontend servers are the receivers.
+/* Preprocess the table in MPC, then start listening and handling secure requests */
+function mpc_preprocess_table(table) {
+    console.log("PREPROCESSING START");
 
-    // First share the size of the array: use threshold 1 so that it is public.
-    jiff_instance.share(array.length, 1, receivers, senders);
+    // Read configurations
+    var config = require('./config.json');
+    
+    // Share the table
+    var backends = [ 1 ]; // Backend server is the only sender and always has ID 1.
+    var frontends = config.frontends; // Frontend servers are the receivers.
 
-    for(var i = 0; i < array.length; i++) // Now share every element of the array
-      jiff_instance.share(array[i], receivers.length, receivers, senders);
+    // First share the length of the table (i.e. number of rows), each row has 3 cols
+    // use threshold 1 so that the sharing is public.
+    jiff_instance.share(table.length, 1, frontends, backends);
+
+     // Now share every element of the table
+    for(var i = 0; i < table.length; i++)
+      for(var j = 0; j < 3; j++)
+        jiff_instance.share(table[i][j], frontends.length, frontends, backends);
 
     // Front end servers will now do some computation, then send the result here.
-    for(var i = 0; i < array.length; i++)
-      array[i] = jiff_instance.share(0, 1, senders, [ receivers[0] ])[receivers[0]];
+    var promises = [];
+    for(var i = 0; i < table.length; i++) {
+      encryptedTable[i] = [];
+
+      // Receive the ith row indices from frontends and open them.
+      // These indices are shuffled and hidden (by utilizing a PRF).
+      promises.push(jiff_instance.receive_open(frontends));
+      promises.push(jiff_instance.receive_open(frontends));
+
+      // Receive each share of the ith table value from the frontends parties, but do not open them.
+      // These shares will be encrypted and stored as is.
+      var encrypted_shares = jiff_instance.share(null, 1, backends, frontends);
+      for(var j = 0; j < table.length; j++)
+        promises.push(encrypted_shares[frontends[j]]);
+    }
+    
+    // Expand results into a table for fast access.
+    Promise.all(promises).then(results) {
+      // encrypted_table will have the form:
+      // encrypted_table[prf(source)][prf(destination)] = [encrypted_share_of_value1, encrypted_share_of_value2, ..]
+      var encrypted_table = {};
+      for(var i = 0; i < table.length; i++)
+        encrypted_table[results[i*3]] = {};
+
+      for(var i = 0; i < table.length; i++) {
+        var encrypted_shares_of_value = [];
+        for(var j = 0; j < frontends.length; j++)
+          encrypted_shares_of_value.push(results[i*3 + 2 + j]);
+
+        encrypted_table[results[i*3]][results[i*3 + 1]] = encrypted_shares_of_value;
+      }
+
+
+      
+    }
 
     // When all the results are received, disconnect.
-    jiff_instance.open_all(array, senders).then(function(results) {
+    jiff_instance.open_all(array, backends).then(function(results) {
         jiff_instance.disconnect();
         console.log(results);
         callback(true);
     });
 }
 
-// Export API
-module.exports = {
-  run: run
-};
--- demos/honda/server.js
+++ demos/honda/server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var jiff_instance = require('../../lib/jiff-server').make_jiff(http, { logs:true });
+var jiff_instance = require('../../lib/jiff-server').make_jiff(http, { logs: false });
 
-// Define a computation with id '1' and a maximum of 4 participants.
-jiff_instance.totalparty_map['1'] = 4;
+// Define a computation with id '1' with 4 participants (max).
+jiff_instance.totalparty_map['shortest-path-1'] = 4;
 
 // Serve static files.
 app.use("/demos", express.static("demos"));
 app.use("/lib", express.static("lib"));
 app.use("/lib/ext", express.static("lib/ext"));
-http.listen(8080, function() {
-  console.log('listening on *:8080');
+http.listen(3000, function() {
+  console.log('listening on *:3000');
 });
 
