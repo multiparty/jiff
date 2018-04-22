8dd8d61b8ada367d4a0c8194100cdcfb76216a3e
Mike Gajda <mgajda@bu.edu>
2018-04-22 11:35:10 -0400

    Relative filenames
    - allow anyone to run the demo with the included json weights

--- demos/neural-net/neural-net.js
+++ demos/neural-net/neural-net.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var math = require('mathjs');
 var numeric = require('numeric/numeric-1.2.6');
 var BigNumber = require('bignumber.js');
+var path = require('path');
 
 var weights_layer_1;
 var biases_layer_1;
@@ -35,13 +36,13 @@ function getSum(total, num) {
 
 
 
-fs.readFile('/users/mikegajda/Documents/keystone_bu_2017_2018/mgajda-khc-keystone/nn_python/weights_layer_1.json', 'utf8', function (err, data) {
+fs.readFile(path.resolve(__dirname, './weights_layer_1.json'), 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
     console.log("weights_layer_1");
     weights_layer_1 = JSON.parse(data);
     console.log(weights_layer_1.length, weights_layer_1[0].length);
 
-    fs.readFile('/users/mikegajda/Documents/keystone_bu_2017_2018/mgajda-khc-keystone/nn_python/weights_out.json', 'utf8', function (err, data) {
+    fs.readFile(path.resolve(__dirname, './weights_out.json'), 'utf8', function (err, data) {
         if (err) throw err; // we'll not consider error handling for now
         console.log("weights_out");
         weights_out = JSON.parse(data);
@@ -66,7 +67,7 @@ function success(result) {
   }
 
 
-var options = {party_count: 2, Zp: new BigNumber(32416190071), offset: 10000, bits: 6, digits: 4  };
+var options = {party_count: 2, Zp: new BigNumber(32416190071), offset: 10000, bits: 7, digits: 4  };
 //var options = {party_count: 2};
 options.onConnect = function() {
     console.log("in onConnect");
@@ -141,94 +142,6 @@ options.onConnect = function() {
         });
 
     });
-
-
-    //     for (var i = 0; i < answer.length; i++){
-    //         answer[i] = answer[i].toFixed(4);
-    //     }
-    //     console.log(answer);
-
-
-    // }, function(failure){
-    //     console.log("failed", failure);
-    // });
-
-            
-    //         var shares_2d = jiff_instance.share_vec(getColumn(test_arr, j));
-
-    //         //var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //         var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //         console.log("results")
-    //         for(var k = 1; k < shares_2d.length; k++) {
-    //             console.log("k = ", k);
-    //             var shares = shares_2d[k];
-    //             var product = shares[1].smult(shares[2]).open().then(success, failure);
-    //             results.push(product);
-
-
-    //         }
-
-    //         product_matrix.push(new Promise(function(resolve, reject) {
-    //           Promise.all(results).then(function(success_result){
-    //             console.log("SUM BEFORE REDUCTIN = ", success_result);
-    //                 resolve(1.0);
-    //             }, function(failure){
-    //                 reject(failure)
-    //             });
-    //         }));
-    //     }
-    // }
-    // Promise.all(product_matrix).then(function(results){
-    //     var test_arr = weights_out;
-
-    //     for (var i = 0; i < test_arr.length; i++){
-    //         for (var j = 0; j < test_arr[0].length; j++){
-    //             test_arr[i][j] = test_arr[i][j].toFixed(4);
-    //             //test_arr[i][j] = 5;
-    //         }
-    //     }
-    //     console.log(test_arr.length, test_arr[0].length)
-       
-    //     var other_array_row_count = 1;
-
-    //     product_matrix = [];
-    //     for(var i = 0; i < test_arr[0].length; i++){
-    //         for (var j = 0; j < other_array_row_count; j++){
-    //             console.log(i, getColumn(test_arr, i));
-    //             var shares_2d = jiff_instance.share_vec(getColumn(test_arr, j));
-
-    //             //var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //             //var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //             var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //             console.log("results")
-    //             for(var k = 1; k < shares_2d.length; k++) {
-    //                 console.log("k = ", k);
-    //                 var shares = shares_2d[k];
-    //                 var product = shares[1].smult(shares[2]).open().then(success, failure);
-    //                 results.push(product);
-
-
-    //             }
-
-    //             product_matrix.push(new Promise(function(resolve, reject) {
-    //               Promise.all(results).then(function(success_result){
-    //                     resolve(success_result.reduce(getSum));
-    //                 }, function(failure){
-    //                     reject(failure)
-    //                 });
-    //             }));
-    //         }
-    //     }
-    //     Promise.all(product_matrix).then(function(results){
-    //         console.log(results);
-
-            
-    //     }, failure);
-
-
-    // }, failure);
-
-    // console.log("done");
 
 }
 
--- demos/neural-net/party.js
+++ demos/neural-net/party.js
@@ -17,24 +17,6 @@ function sigmoid(t) {
     return 1/(1+Math.pow(Math.E, -t));
 }
 
-
-
-
-fs.readFile('/users/mikegajda/Documents/keystone_bu_2017_2018/mgajda-khc-keystone/nn_python/weights_layer_1.json', 'utf8', function (err, data) {
-    if (err) throw err; // we'll not consider error handling for now
-    console.log("weights_layer_1");
-    weights_layer_1 = JSON.parse(data);
-    console.log(weights_layer_1.length, weights_layer_1[0].length);
-
-    fs.readFile('/users/mikegajda/Documents/keystone_bu_2017_2018/mgajda-khc-keystone/nn_python/weights_out.json', 'utf8', function (err, data) {
-        if (err) throw err; // we'll not consider error handling for now
-        console.log("weights_out");
-        weights_out = JSON.parse(data);
-        console.log(weights_out.length, weights_out[0].length);
-
-    }); 
-});
-
 function success(result) { 
     console.log("success, result = " + result);
     return result;
@@ -52,7 +34,7 @@ function success(result) {
     console.log("in getSum")
     return total + num;
 }
-var options = {party_count: 2, Zp: new BigNumber(32416190071), offset: 10000, bits: 6, digits: 4  };
+var options = {party_count: 2, Zp: new BigNumber(32416190071), offset: 10000, bits: 7, digits: 4  };
 
 //var options = {party_count: 2};
 options.onConnect = function() {
@@ -129,6 +111,7 @@ options.onConnect = function() {
                 answer.push(single_result.toFixed(4));
             }
             console.log(answer.map(sigmoid));
+            console.log(answer.map(sigmoid).map(round));
             console.log("now done");
 
         }, function(failure){
@@ -141,94 +124,6 @@ options.onConnect = function() {
     }, function(failure){
         console.log("failed", failure);
     });
-
-    // for (var i = 0; i < test_arr.length; i++){
-    //     for (var j = 0; j < other_array_col_count; j++){
-    //         console.log(i, j, test_arr[i]);
-    //         var shares_2d = jiff_instance.share_vec(test_arr[i]);
-    //         console.log(shares_2d.length, shares_2d[0].length)
-            
-    //         //var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //         var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //         console.log("results")
-    //         for(var k = 1; k < shares_2d.length; k++) {
-    //             console.log("k = ", k);
-    //             var shares = shares_2d[k];
-    //             var product = shares[1].smult(shares[2]).open().then(success, failure);
-    //             results.push(product);
-
-
-    //         }
-
-    //         product_matrix.push(new Promise(function(resolve, reject) {
-    //           Promise.all(results).then(function(success_result){
-    //                 console.log("made it here", success_result);
-    //                 sum = 0;
-    //                 for (var i = 0; i < success_result.length; i++){
-    //                     sum += success_result[i].toNumber();
-    //                 }
-    //                 console.log(sum.toFixed(4));
-    //                 resolve(sum.toFixed(4));
-    //             }, function(failure){
-    //                 reject(failure)
-    //             });
-    //         }));
-            
-            
-    //     }
-    // }
-    // Promise.all(product_matrix).then(function(results){
-    //     console.log(results);
-    //     console.log("first done, i'm ")
-
-    //     test_arr = [results];
-
-    //     console.log(test_arr.length, test_arr[0].length);
-
-    //     // we know the array we're multiplying by is 35 x 75
-    //     var other_array_col_count = 3;
-
-    //     product_matrix = [];
-
-    //     for (var i = 0; i < test_arr.length; i++){
-    //         for (var j = 0; j < other_array_col_count; j++){
-    //             console.log(i, j, test_arr[i]);
-    //             var shares_2d = jiff_instance.share_vec(test_arr[i]);
-    //             console.log(shares_2d.length, shares_2d[0].length)
-                
-    //             //var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //             var results = [shares_2d[0][1].smult(shares_2d[0][2]).open().then(success, failure)];
-    //             console.log("results here")
-    //             for(var k = 1; k < shares_2d.length; k++) {
-    //                 console.log("k = ", k);
-    //                 var shares = shares_2d[k];
-    //                 var product = shares[1].smult(shares[2]).open().then(success, failure);
-    //                 results.push(product);
-
-
-    //             }
-
-    //             product_matrix.push(new Promise(function(resolve, reject) {
-    //               Promise.all(results).then(function(success_result){
-    //                     console.log("made it here", success_result);
-    //                     succ
-    //                     resolve(success_result.reduce(getSum));
-    //                 }, function(failure){
-    //                     reject(failure)
-    //                 });
-    //             }));
-                
-                
-    //         }
-    //     }
-    //     Promise.all(product_matrix).then(function(results){
-    //         for (var i = 0; i < result.length; i++){
-    //             results[i] = results[i].toNumber();
-    //         }
-    //         results = [results];
-    //         console.log(results);
-    //     }, failure);
-    // }, failure);
 
     console.log("done");
 
