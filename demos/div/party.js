7e90a98ccaad215946fb7a069d925a67fa3d2dc3
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2018-05-23 23:11:50 -0400

    Faster div demo

--- demos/div/party.js
+++ demos/div/party.js
@@ -16,21 +16,57 @@ if(party_id != null) party_id = parseInt(party_id, 10);
 // JIFF options
 var options = {party_count: party_count, party_id: party_id};
 options.onConnect = function(jiff_instance) {
-/*
-  var divBy = 100;
-  
-  Math.floor(jiff_instance.Zp / divBy)
-
+  try {
   var shares = jiff_instance.share(input);
   var x = shares[1];
-  var noise1 = jiff_instance.server_generate_and_share( {"max":  } );
-  var noise2 = jiff_instance.server_generate_and_share( {"max": } );
-  var noise = noise1.sadd(noise2);
-  */
   
-  var shares = jiff_instance.share(input);
-  shares[1].sdiv(shares[2]).open(console.log);
-}
+  var c = 100;  
+  var ZpOVERc = Math.floor(jiff_instance.Zp / c);
+
+  var nOVERc = jiff_instance.server_generate_and_share( {"max":  Math.floor(ZpOVERc / 100)} );
+  var nMODc = jiff_instance.server_generate_and_share( {"max": c, "nonzero": true} );
+  var noise = nOVERc.cmult(c).sadd(nMODc);
+  
+  var noisyX = x.sadd(noise);
+  var wrapped = x.sgt(noisyX, null, "<1");
+
+  noisyX.open(function(noisyX) {
+    noisyX = Math.floor(noisyX/c);
+
+    // if did not wrap
+    var unCorrectedQuotient = nOVERc.cmult(-1).cadd(noisyX);
+    var verifyX = unCorrectedQuotient.cmult(c);
+    var isNotCorrect = verifyX.sgt(x, null, "<2");
+    var noWrapAnswer = unCorrectedQuotient.ssub(isNotCorrect); // if incorrect => isNotCorrect = 1 => quotient = unCorrectedQuotient - 1   
+
+    // if wrapped
+    var unCorrectedQuotient = nOVERc.cmult(-1).cadd(noisyX).cadd(ZpOVERc);
+    var q1 = unCorrectedQuotient.csub(2);
+    var q2 = unCorrectedQuotient.csub(1);
+    var q3 = unCorrectedQuotient.csub(0);
+    var q4 = unCorrectedQuotient.cadd(1);
+    
+    var q4Correct = q4.cmult(c).slt(x);
+    var q3Correct = q3.cmult(c).slt(x);
+    var q2Correct = q2.cmult(c).slt(x);
+    var q1Correct = q1.cmult(c).slt(x);
+    
+    q3Correct = q3Correct.smult(q4Correct.not());
+    q2Correct = q2Correct.smult(q3Correct.not());
+    q1Correct = q1Correct.smult(q2Correct.not());
+
+    var wrapAnswer = q4.smult(q4Correct);
+    wrapAnswer = wrapAnswer.sadd(q3.smult(q3Correct));
+    wrapAnswer = wrapAnswer.sadd(q2.smult(q2Correct));
+    wrapAnswer = wrapAnswer.sadd(q1.smult(q1Correct));
+
+    var answer = noWrapAnswer.sadd(wrapped.smult(wrapAnswer.ssub(noWrapAnswer)));
+    answer.open(console.log);
+  });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // Connect
 var jiff_instance = require('../../lib/jiff-client').make_jiff("http://localhost:8080", computation_id, options);
