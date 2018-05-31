75e8f4105b4d88321b75fc347eee1301921a8307
842Mono <842mono@gmail.com>
2018-05-31 19:26:55 -0400

    "border controls"

--- demos/differential-privacy/display.js
+++ demos/differential-privacy/display.js
@@ -1,11 +1,9 @@
 // the limits of the graph
-const minX = 0;
-const maxX = 10;
-const minY = 0;
-const maxY = 25;
+let minX1 = 0;
+let maxX1 = 10;
+let minY1 = 0;
+let maxY1 = 25;
 
-
-// array holds the coordinates input by the user.
 const points1 = [];
 const datasetPoints1 = {
     id:"data-points",
@@ -35,16 +33,16 @@ const chart1 = new Chart(ctx1, {
         scales: {
             yAxes: [{
                 ticks: {
-                    min: minY,
-                    max: maxY
+                    min: minY1,
+                    max: maxY1
                 }
             }],
             xAxes: [{
                 type: 'linear',
                 position: 'bottom',
                 ticks: {
-                    min: minX,
-                    max: maxX,
+                    min: minX1,
+                    max: maxX1,
                     maxTicksLimit: 20
                 }
             }]
@@ -52,6 +50,12 @@ const chart1 = new Chart(ctx1, {
     }
 });
 
+
+// the limits of the graph
+let minX2 = 0;
+let maxX2 = 10;
+let minY2 = 0;
+let maxY2 = 25;
 
 const points2 = [];
 const datasetPoints2 = {
@@ -82,16 +86,16 @@ const chart2 = new Chart(ctx2, {
         scales: {
             yAxes: [{
                 ticks: {
-                    min: minY,
-                    max: maxY
+                    min: minY2,
+                    max: maxY2
                 }
             }],
             xAxes: [{
                 type: 'linear',
                 position: 'bottom',
                 ticks: {
-                    min: minX,
-                    max: maxX,
+                    min: minX2,
+                    max: maxX2,
                     maxTicksLimit: 20
                 }
             }]
@@ -105,19 +109,69 @@ const chart2 = new Chart(ctx2, {
  * @param {object} points - The json object containing the two points {raw:{x,y},noisy:{x,y}}
  */
 const displayPointsOnTwoGraphs = function(points) {
+    // sainity checks
     if(isNaN(points.raw.x) || isNaN(points.raw.y) || isNaN(points.noisy.x) || isNaN(points.noisy.y)) {
-        alert("One of the values isNaN");
+        alert("One of the values is not a number.");
         console.warn(points);
         return;
     }
+    
+    // border controls
+    minX1 = points.raw.x < minX1 ? points.raw.x+10 : minX1;
+    maxX1 = points.raw.x > maxX1 ? points.raw.x+10 : maxX1;
+    minY1 = points.raw.y < minY1 ? points.raw.y+10 : minY1;
+    maxY1 = points.raw.y > maxY1 ? points.raw.y+10 : maxY1;
+    minX2 = points.noisy.x < minX2 ? points.noisy.x+10 : minX2;
+    maxX2 = points.noisy.x > maxX2 ? points.noisy.x+10 : maxX2;
+    minY2 = points.noisy.y < minY2 ? points.noisy.y+10 : minY2;
+    maxY2 = points.noisy.y > maxY2 ? points.noisy.y+10 : maxY2;
+    console.log(minX1, maxX1, minY1, maxY1, minX2, maxX2, minY2, maxY2)
+    chart1.options.scales = {
+        yAxes: [{
+            ticks: {
+                min: minY1,
+                max: maxY1
+            }
+        }],
+        xAxes: [{
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+                min: minX1,
+                max: maxX1,
+                maxTicksLimit: 20
+            }
+        }]
+    }
+    chart2.options.scales = {
+        yAxes: [{
+            ticks: {
+                min: minY2,
+                max: maxY2
+            }
+        }],
+        xAxes: [{
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+                min: minX2,
+                max: maxX2,
+                maxTicksLimit: 20
+            }
+        }]
+    }
 
+    // display the input points
     points1.push(points.raw);
     points2.push(points.noisy);
     
+    // display the average
     const avg1 = points1.reduce((acc,curr) => acc+curr.y, 0) / points1.length;
     const avg2 = points2.reduce((acc,curr) => acc+curr.y, 0) / points2.length;
-    datasetLines1.data = [{x:minX, y:avg1}, {x:maxX, y:avg1}];
-    datasetLines2.data = [{x:minX, y:avg2}, {x:maxX, y:avg2}];
+    datasetLines1.data = [{x:minX1, y:avg1}, {x:maxX1, y:avg1}];
+    datasetLines2.data = [{x:minX2, y:avg2}, {x:maxX2, y:avg2}];
+
+    // finally update the charts
     chart1.update();
     chart2.update();
 }
