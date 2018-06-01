650364ae7d823fec074f7745c5ba4a6fb5d55a4e
Kinan Bab <KinanBab@users.noreply.github.com>
2018-06-01 10:27:35 -0400

    Fix up nodejs template

--- demos/template/party.js
+++ demos/template/party.js
@@ -16,11 +16,7 @@ if(party_id != null) party_id = parseInt(party_id, 10);
 // JIFF options
 var options = {party_count: party_count, party_id: party_id};
 options.onConnect = function(jiff_instance) {
-  var shares = jiff_instance.share(input);
-  var sum = shares[1];
-  for(var i = 2; i <= jiff_instance.party_count; i++)
-    sum = sum.sadd(shares[i]);
-  sum.open(function(v) { console.log(v); jiff_instance.disconnect(); });
+  /** YOUR CODE GOES HERE **/
 }
 
 // Connect
