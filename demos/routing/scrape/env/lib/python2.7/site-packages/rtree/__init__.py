25b1b2d815fe1e3ddb7e53c60e0853c9467941b8
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2018-06-03 20:40:21 -0400

    cleanup

--- demos/routing/scrape/env/lib/python2.7/site-packages/rtree/__init__.py
+++
@@ -1,5 +0,0 @@
-from .index import Rtree
-
-from .core import rt
-
-__version__ = '0.8.3'
--- demos/routing/scrape/env/lib/python2.7/site-packages/shapely/validation.py
+++
@@ -1,8 +0,0 @@
-# TODO: allow for implementations using other than GEOS
-
-import sys
-
-from shapely.geos import lgeos
-
-def explain_validity(ob):
-    return lgeos.GEOSisValidReason(ob._geom)
