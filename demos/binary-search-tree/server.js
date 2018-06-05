3b3bc7f8c659e6d291fae365a4db27c18265b66d
lucyq <lucyzqin@gmail.com>
2018-06-04 22:17:52 -0400

    changed port to 8080 on bst demo

--- demos/binary-search-tree/server.js
+++ demos/binary-search-tree/server.js
@@ -7,6 +7,6 @@ var jiff_instance = require('../../lib/jiff-server').make_jiff(http, {logs:true}
 app.use("/demos", express.static("demos"));
 app.use("/lib", express.static("lib"));
 app.use("/lib/ext", express.static("lib/ext"));
-http.listen(8000, function() {
-  console.log('listening on *:8000');
+http.listen(8080, function() {
+  console.log('listening on *:8080');
 }); 
