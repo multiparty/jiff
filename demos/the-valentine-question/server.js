038e80f884bd5a2951e65632f7c5dda4da1e0b9a
lucyq <lucyzqin@gmail.com>
2018-06-04 22:12:01 -0400

    changing port number to 8080 for the valentine question demo

--- demos/the-valentine-question/server.js
+++ demos/the-valentine-question/server.js
@@ -7,6 +7,6 @@ var jiff_instance = require('../../lib/jiff-server').make_jiff(http, {logs:true}
 app.use("/demos", express.static("demos"));
 app.use("/lib", express.static("lib"));
 app.use("/lib/ext", express.static("lib/ext"));
-http.listen(7000, function() {
-  console.log('listening on *:7000');
+http.listen(8080, function() {
+  console.log('listening on *:8080');
 });
