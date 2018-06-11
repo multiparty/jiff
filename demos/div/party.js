e539d24c8596a528e289818bb405292d133e16ab
Kinan Dak Albab <Kinan.bab.14@gmail.com>
2018-06-11 14:37:10 -0400

    Nodejs version of constant division demo

--- demos/div/party.js
+++ demos/div/party.js
@@ -23,41 +23,27 @@ options.onConnect = function(jiff_instance) {
   var c = 100;  
   var ZpOVERc = Math.floor(jiff_instance.Zp / c);
 
-  var nOVERc = jiff_instance.server_generate_and_share( {"max":  Math.floor(ZpOVERc / 100)} );
-  var nMODc = jiff_instance.server_generate_and_share( {"max": c, "nonzero": true} );
+  var nOVERc = jiff_instance.server_generate_and_share( { "max":  ZpOVERc } );
+  var nMODc = jiff_instance.server_generate_and_share( { "max": c } );
   var noise = nOVERc.cmult(c).sadd(nMODc);
   
   var noisyX = x.sadd(noise);
   noisyX.open(function(noisyX) {
-    var wrapped = x.cgt(noisyX);
-    noisyX = Math.floor(noisyX/c);
+    var wrapped = x.cgt(noisyX); // 1 => x + noise wrapped around Zp, 0 otherwise
 
-    // if did not wrap
-    var unCorrectedQuotient = nOVERc.cmult(-1).cadd(noisyX);
-    var verifyX = unCorrectedQuotient.cmult(c);
-    var isNotCorrect = verifyX.sgt(x);
-    var noWrapAnswer = unCorrectedQuotient.ssub(isNotCorrect); // if incorrect => isNotCorrect = 1 => quotient = unCorrectedQuotient - 1   
+    // if we did not wrap
+    var noWrapDiv = Math.floor(noisyX/c);
+    var unCorrectedQuotient = nOVERc.cmult(-1).cadd(noWrapDiv).csub(1);
+    var verify = x.ssub(unCorrectedQuotient.cmult(c));
+    var isNotCorrect = verify.cgteq(c);
+    var noWrapAnswer = unCorrectedQuotient.sadd(isNotCorrect); // if incorrect => isNotCorrect = 1 => quotient = unCorrectedQuotient - 1   
 
-    // if wrapped
-    var unCorrectedQuotient = nOVERc.cmult(-1).cadd(noisyX).cadd(ZpOVERc);
-    var q1 = unCorrectedQuotient.csub(2);
-    var q2 = unCorrectedQuotient.csub(1);
-    var q3 = unCorrectedQuotient.csub(0);
-    var q4 = unCorrectedQuotient.cadd(1);
-    
-    var q4Correct = q4.cmult(c).slt(x);
-    var q3Correct = q3.cmult(c).slt(x);
-    var q2Correct = q2.cmult(c).slt(x);
-    var q1Correct = q1.cmult(c).slt(x);
-    
-    q3Correct = q3Correct.smult(q4Correct.not());
-    q2Correct = q2Correct.smult(q3Correct.not());
-    q1Correct = q1Correct.smult(q2Correct.not());
-
-    var wrapAnswer = q4.smult(q4Correct);
-    wrapAnswer = wrapAnswer.sadd(q3.smult(q3Correct));
-    wrapAnswer = wrapAnswer.sadd(q2.smult(q2Correct));
-    wrapAnswer = wrapAnswer.sadd(q1.smult(q1Correct));
+    // if we wrapped
+    var wrapDiv = Math.floor((noisyX + jiff_instance.Zp)/c);
+    unCorrectedQuotient = nOVERc.cmult(-1).cadd(wrapDiv).csub(1);
+    verify = x.ssub(unCorrectedQuotient.cmult(c));
+    isNotCorrect = verify.cgteq(c);
+    var wrapAnswer = unCorrectedQuotient.sadd(isNotCorrect); // if incorrect => isNotCorrect = 1 => quotient = unCorrectedQuotient - 1  
 
     var answer = noWrapAnswer.sadd(wrapped.smult(wrapAnswer.ssub(noWrapAnswer)));
     answer.open(console.log);
