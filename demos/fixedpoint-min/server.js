740cc9e7e03572cef814a1044907319bb14fc12a
Kinan Dak Albab <Kinan.bab.14@gmail.com>
2018-06-25 11:44:00 -0400

    Templify and test fixedpoint-min demo

---
+++ demos/fixedpoint-min/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var base_instance = require('../../lib/jiff-server').make_jiff(http, { logs:true });
+var jiff_instance = require('../../lib/ext/jiff-server-bignumber').make_jiff(base_instance);
+
+// Serve static files.
+app.use("/demos", express.static("demos"));
+app.use("/lib", express.static("lib"));
+app.use("/lib/ext", express.static("lib/ext"));
+app.use("/bignumber.js", express.static("node_modules/bignumber.js"));
+http.listen(8080, function() {
+  console.log('listening on *:8080');
+});
+
+console.log("Direct your browser to *:8080/demos/fixedpoint-min/client.html.");
+console.log("To run a server-based party: node demos/fixedpoint-min/party <input");
+console.log();
