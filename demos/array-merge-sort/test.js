cfc33870d568f8cba0f2e72ac366f544c1d1909a
lucyq <lucyzqin@gmail.com>
2018-07-10 14:27:23 -0400

    exhaustive tests

--- demos/array-merge-sort/test.js
+++ demos/array-merge-sort/test.js
@@ -1,15 +1,15 @@
 // Chai 
 var assert = require('chai').assert;
-
 var mpc = require('./mpc.js');
 
 // Generic Testing Parameters
-var party_count = 2;
-var n = 8;
+var party_count = 4;
+var parallelismDegree = 2; // Max number of test cases running in parallel
+var n = 4;
 
 // Parameters specific to this demo
-var maxValue = 1000;
-
+var maxElement = 1000;
+var maxLength = 4;
 
 /**
  * CHANGE THIS: Generate inputs for your tests
@@ -18,19 +18,23 @@ var maxValue = 1000;
  *   'party_id': [ 'test1_input', 'test2_input', ...]
  * }
  */
-function generateInputs(party_count, inputSize) {
-
+function generateInputs(party_count) {
   var inputs = {};
+  for (var i = 1; i <= party_count; i++)
+    inputs[i] = [];
 
-  for (var i = 0; i < party_count; i++) {
-    inputs[i+1] = [];
-  }
   
-  for (var i = 0; i < party_count; i++) {
-    for (var j = 0; j < inputSize; j++) {
-      inputs[i+1].push(Math.floor((Math.random() * maxValue)));
-    }  
+  for (var t = 0; t < n; t++) {
+    var length = Math.floor(Math.random() * maxLength) + 1;
+    for (var p = 1; p <= party_count; p++) {  
+      var arr = [];
+      while (arr.length < length)
+        arr.push(Math.floor(Math.random() * maxElement));
+
+      inputs[p][t] = arr;
+    }
   }
+
   return inputs;
 }
 
@@ -41,27 +45,24 @@ function generateInputs(party_count, inputSize) {
  *   [ 'test1_output', 'test2_output', ... ]
  */
 function computeResults(inputs) {
-  const parties = Object.keys(inputs);
+  var results = [];
 
-  const len = inputs[parties[0]].length;
+  for (var t = 0; t < n; t++) {
+    var array = inputs[1][t].slice(); // Shallow copy, so that when modifying things are not changed!
 
-  let results = [];
-
-  for (let i = 0; i < len; i++) {
-    results.push(0);
-  }
-
-  for (let p in parties) {
-
-    var values = inputs[parties[p]];
-
-    for (let i = 0; i < len; i++) {
-      results[i] = results[i] + values[i];
+    for (var p = 2; p <= party_count; p++) {
+      var tmp = inputs[p][t];
+      for (var i = 0; i < array.length; i++)
+        array[i] += tmp[i];
     }
+
+    array = array.sort(function(a, b) { return a - b; });
+    results.push(array);
   }
 
-  return results.sort();
+  return results;
 }
+
 
 /**
  * Do not change unless you have to.
@@ -69,49 +70,54 @@ function computeResults(inputs) {
 describe('Test', function() {
   this.timeout(0); // Remove timeout
 
-  it('Single test', function(done) {
+  it('Exhaustive', function(done) {
     var count = 0;
 
-    var inputs = generateInputs(party_count, 8);
+    var inputs = generateInputs(party_count);
     var realResults = computeResults(inputs);
 
     var onConnect = function(jiff_instance) {
       var partyInputs = inputs[jiff_instance.id];
-    
-      let promises = [];
 
-      promises.push(mpc.compute(partyInputs, jiff_instance));
+      var testResults = [];      
+      (function one_test_case(j) {
+        if(j < partyInputs.length) {
+          var promises = [];
+          for(var t = 0; t < parallelismDegree && (j + t) < partyInputs.length; t++)
+            promises.push(mpc.compute(partyInputs[j+t], jiff_instance));
 
-      let testResults = [];
-      
-      Promise.all(promises).then(function(results) {
-        testResults = results;
-      });
-    
-      // If we reached here, it means we are done
-      count++;
+          Promise.all(promises).then(function(parallelResults) {
+            for(var t = 0; t < parallelResults.length; t++)
+              testResults.push(parallelResults[t]);
 
-      for (var i = 0; i < testResults.length; i++) {
-        // construct debugging message
-        var ithInputs = inputs[1][i] + "";
-        for (var j = 2; j <= party_count; j++)
-          ithInputs += "," + inputs[j][i];
-        var msg = "Party: " + jiff_instance.id + ". inputs: [" + ithInputs + "]";
+            one_test_case(j+parallelismDegree);
+          });
 
-        // assert results are accurate
-        try {
-          assert.deepEqual(testResults, realResults, msg);
-        } catch(assertionError) {
-          done(assertionError);
-          done = function(){}
+          return;
         }
-      }
 
-      jiff_instance.disconnect();
+        // If we reached here, it means we are done
+        count++;
+        for (var i = 0; i < testResults.length; i++) {
+          // construct debugging message
+          var ithInputs = inputs[1][i] + "";
+          for (var j = 2; j <= party_count; j++)
+            ithInputs += "," + inputs[j][i];
+          var msg = "Party: " + jiff_instance.id + ". inputs: [" + ithInputs + "]";
 
-      if (count == party_count) {
-        done();
-      }
+          // assert results are accurate
+          try {
+            assert.deepEqual(testResults[i], realResults[i], msg);
+          } catch(assertionError) {
+            done(assertionError);
+            done = function(){}
+          }
+        }
+
+        jiff_instance.disconnect();
+        if (count == party_count)
+          done();
+      })(0);
     };
     
     var options = { party_count: party_count, onError: console.log, onConnect: onConnect };
