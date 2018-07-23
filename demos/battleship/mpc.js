768d426904a2425c5589d28c9f26153458e63511
Gati Aher <2gatiaher@gmail.com>
2018-07-23 10:49:52 -0400

    now takes about 45 seconds per turn because Zp = 89.

--- demos/battleship/mpc.js
+++ demos/battleship/mpc.js
@@ -14,7 +14,7 @@
      */
     exports.connect = function (hostname, computation_id, options) {
       var opt = Object.assign({}, options);
-  
+      opt.Zp = 89;
       if(node)
         jiff = require('../../lib/jiff-client');
   
