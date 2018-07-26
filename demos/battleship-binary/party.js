a44446f692acc637e32beda36acda2264736e2c6
Gati Aher <2gatiaher@gmail.com>
2018-07-26 15:48:47 -0400

    saving.

--- demos/battleship-binary/party.js
+++ demos/battleship-binary/party.js
@@ -6,7 +6,7 @@
 
 // console.log("Command line arguments: <input> [<party count> [<computation_id> [<party id>]]]]");
 console.log("Command line arguments: <ships> [<guesses> [<computation_id> [<party id>]]]]");
-console.log("Use party id 1 when providing the haystack, and party id 2 when providing needle.")
+console.log("Party id can be 1 or 2");
 
 var mpc = require('./mpc');
 
