15a419a5ac9f6c7c10e860f65e471c0cfb6c2de0
Gati Aher <2gatiaher@gmail.com>
2018-08-02 10:01:19 -0400

    started making a configured version of battleships -- users can choose which one to use. Some of the version names will be chaged for more clarity

---
+++ demos/battleship-all/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app); // server
+var jiff_instance = require('../../lib/jiff-server').make_jiff(http, { logs:true });
+
+// Serve static files.
+app.use("/demos", express.static("demos"));
+app.use("/lib", express.static("lib"));
+app.use("/lib/ext", express.static("lib/ext"));
+
+http.listen(8080, function() {
+  console.log('listening on *:8080');
+});
+
+console.log("Direct your browser to *:8080/demos/battleship-all/client.html.");
+console.log("To run a node.js based party: node demos/battleship-all/party, Command line arguments: <ships> <guesses> [<computation_id>] [<party id>]");
+console.log();
