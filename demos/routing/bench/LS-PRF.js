2143080ab5c10c081603a3327028e94893790e4c
Kinan Dak Albab <Kinan.bab.14@gmail.com>
2018-08-21 17:32:37 -0400

    Clean up LS bench

--- demos/routing/bench/LS-PRF.js
+++ demos/routing/bench/LS-PRF.js
@@ -1,3 +1,22 @@
+// PRF Count to benchmark
+var count = process.argv[2];
+var keyLength = process.argv[3];
+if(count == null) count = 40000;
+if(keyLength == null) keyLength = 30;
+
+// Constants
+var prime = 2860486313;
+var inv2 = (prime + 1) / 2;
+var power = (prime - 1) / 2;
+
+// Keys
+var keys = [];
+for(var i = 0; i < keyLength; i++) {
+  keys[i] = Math.random() * prime;
+  keys[i] = Math.floor(keys[i]);
+}
+
+// Fast exponentiation
 var expmod = function(a, b, n) {
   a = a % n;
   var result = 1;
@@ -18,16 +37,7 @@ var expmod = function(a, b, n) {
   return result;
 };
 
-var prime = 2860486313;
-var inv2 = (prime + 1) / 2;
-var power = (prime - 1) / 2;
-
-var keys = [];
-for(var i = 0; i < 30; i++) {
-  keys[i] = Math.random() * prime;
-  keys[i] = Math.floor(keys[i]);
-}
-
+// PRF
 function applyPRF(value) {
   var result = 0; // Each Key gives us a bit of the result
   for(var i = 0; i < keys.length; i++) {
@@ -43,26 +53,19 @@ function applyPRF(value) {
   return result;
 }
 
-// PRF Count to benchmark
-var count = process.argv[2];
-if(count == null) count = 200;
-
+// Benchmarks
 var elements = new Set();
-var counter = 0;
 for(var i = 1; i <= count; i++) {
-  for(var j = 1; j <= count; j++) {
-    counter++;
-    var element = counter;
-    var garbled = applyPRF(element).toString(); 
-    
-    if(elements.has(garbled)) {
-      console.log('colision');
-      return;
-    }
-
-    elements.add(garbled);
+  var garbled = applyPRF(i); 
+  
+  if(elements.has(garbled)) {
+    console.log('colision');
+    return;
   }
+
+  elements.add(garbled);
+  console.log(i, garbled);
 }
 
-console.log('done ');
+console.log('done');
 
