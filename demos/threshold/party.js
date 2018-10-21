a424cd6627ec40de657b2a992e0d8be059e52912
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2018-10-21 19:46:49 -0400

    clean up threshold and add stress test bash script: TODO: mocha tests

--- demos/threshold/party.js
+++ demos/threshold/party.js
@@ -1,110 +1,45 @@
 /**
- * Implemented based on Practical Accountability of Secret Processes, Frankle et al.
- * This code parses input command line arguments, 
+ * Do not change this unless you have to.
+ * This code parses input command line arguments,
  * and calls the appropriate initialization and MPC protocol from ./mpc.js
  */
-
-console.log("Command line arguments: <input> <upper party count> <lower party count> <aggregate_bool> <threshold_val> <party id> <computation_id>");
+if (process.argv[8] == null) {
+  console.log('Command line arguments: <input> <threshold> <lower_party_count> <upper_party_count> [<computation_id> [<party id>]]');
+}
 
 var mpc = require('./mpc');
-var BigNumber = require('bignumber.js');
 
-// Read Command line arguments from the initial connection of the lower parties.
-var input = parseInt(process.argv[2], 10); // this is the mpc input
+// Read Command line arguments
+var input = parseInt(process.argv[2], 10);
+var threshold = parseInt(process.argv[3], 10);
+var lower_count = parseInt(process.argv[4], 10);
+var upper_count = parseInt(process.argv[5], 10);
 
-// The number of upper parties.
-var upper_party_count = process.argv[3];
-if(upper_party_count == null) upper_party_count = 2;
-else upper_party_count = parseInt(upper_party_count);
-
-// The number of lower parties.
-var lower_party_count = process.argv[4];
-if(lower_party_count == null) lower_party_count = 3;
-else lower_party_count = parseInt(lower_party_count);
-
-// The number of total parties.
-var party_count = upper_party_count + lower_party_count;
-
-// Create upper party ids.
-var upper_ids = [];
-for (var i = 1; i <= upper_party_count; i++) {
-  upper_ids.push(i);
+var computation_id = process.argv[6];
+if (computation_id == null) {
+  computation_id = 'threshold-test';
 }
 
-// Create lower party ids.
-var lower_ids = [];
-for (var j = upper_party_count + 1;  j <= party_count; j++) {
-  lower_ids.push(j);
-}
-
-// boolean (is additive thresholding desired? Otherwise, multiplicative thresholding will be computed.)
-// Defaults to multiplicative thresholding
-var aggregate = process.argv[5];
-if(aggregate != null) aggregate = JSON.parse(aggregate);
-else aggregate = false;
-
-// Value of the threshold to compare.
-var threshold_val = process.argv[6];
-if(threshold_val != null) threshold_val = parseInt(threshold_val, 10);
-else threshold_val = 10;
-
-// Each judge must have a different party id. We assume they know what this is.
 var party_id = process.argv[7];
-if(party_id != null) party_id = parseInt(party_id, 10);
+if (party_id != null) {
+  party_id = parseInt(party_id, 10);
+}
 
-var computation_id = process.argv[8];
-if(computation_id == null) computation_id = 'test-threshold';
+var upper_parties = ['s1'];
+for (var i = lower_count + 1; i <= upper_count + lower_count; i++) {
+  upper_parties.push(i);
+}
 
 // JIFF options
-var options = {party_count: party_count, party_id: party_id, Zp: "1208925819614629174706111"};
-options.onConnect = function(jiff_instance) {
-
-  // Lower party secret sharing
-  if (party_id > upper_party_count) {
-    var shares = jiff_instance.share(input, upper_party_count, upper_ids, lower_ids); // each judge creates a secret share out of their input and sends to upper
-    console.log("Inputs shared. Disconnecting.")
-
-    // Create array of share promises
-    var send_wait = [];
-    for (var k = lower_party_count; k <= party_count; k++) {
-      send_wait.push(shares[k]);
-    }
-
-    // When all shares are sent, the lower party may disconnect
-    Promise.all(send_wait).then(function () {
-      jiff_instance.disconnect();
-    });
-
-  } else {
-    // Upper party MPC computation
-    var received_shares = jiff_instance.share(null, upper_party_count, upper_ids, lower_ids);
-
-    // Create array of received share promises
-    var received_wait = [];
-    for (var g =lower_party_count; g <= party_count; g++) {
-      received_wait.push(received_shares[g]);
-    }
-
-    // Once all shares are received, the upper party can go into the MPC computation
-    Promise.all(received_wait).then(function (shares) {
-      console.log("Shares from lower parties received. Computing MPC...");
-      var promise = mpc.compute(shares, aggregate, threshold_val);
-
-      // When the MPC is done, we print the result
-      promise.then(function (v) {
-        if (aggregate) {
-          console.log("The number of parties that exceed the threshold is: ", v.toString());
-        } else {
-          if (!!v) console.log("All parties exceed the given threshold.");
-          else console.log("Not all parties exceed the given threshold.");
-        }
-
-        jiff_instance.disconnect();
-      });
-
-    });
-  }
-};
+var options = { party_count: lower_count + upper_count, party_id: party_id, Zp: 127 };
 
 // Connect
-mpc.connect("http://localhost:8080", computation_id, options);
+var jiff_instance = mpc.connect('http://localhost:8080', computation_id, options);
+jiff_instance.wait_for(upper_parties, function () {
+  var promise = mpc.compute({ value: input, threshold: threshold, upper_count: upper_count, lower_count: lower_count });
+
+  promise.then(function (v) {
+    console.log(v);
+    jiff_instance.disconnect(true);
+  });
+});
--- demos/threshold/server.js
+++ demos/threshold/server.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var base_instance = require('../../lib/jiff-server').make_jiff(http, { logs:true });
-var jiff_instance = require('../../lib/ext/jiff-server-bignumber').make_jiff(base_instance);
+require('../../lib/jiff-server').make_jiff(http, { logs:false });
 
-// Server static files.
-app.use("/demos", express.static("demos"));
-app.use("/lib", express.static("lib"));
-app.use("/lib/ext", express.static("lib/ext"));
-app.use("/bignumber.js", express.static("node_modules/bignumber.js"));
-
-http.listen(8080, function() {
-  console.log('listening on *:8080');
+// Serve static files.
+app.use('/demos', express.static('demos'));
+app.use('/lib', express.static('lib'));
+app.use('/lib/ext', express.static('lib/ext'));
+http.listen(8080, function () {
+  if (process.argv[2] !== 'suppress') {
+    console.log('listening on *:8080');
+  }
 });
 
-console.log("Direct your browser to *:8080/demos/threshold/client.html.");
-console.log("To run a server-based party: node demos/threshold/party <input>");
-console.log();
+if (process.argv[2] !== 'suppress') {
+  console.log('Direct your browser to *:8080/demos/threshold/client.html.');
+  console.log('To run a server-based party: node demos/threshold/party <input');
+  console.log();
+}
