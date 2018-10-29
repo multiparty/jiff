34483cfbe07baba0f172d719055b51f113b2ccf1
p-flock <peter.flockhart@gmail.com>
2018-10-29 14:30:35 -0400

    Rename share.change_threshold() to .reshare() in jiff-client. Supports earlier functionality used by smult_bgw and also can be used to distribute existing shares to new parties.

--- demos/pre/mpc.js
+++ demos/pre/mpc.js
@@ -22,36 +22,45 @@
     if (jiff_instance == null) {
       jiff_instance = saved_instance;
     }
+    /*var my_input = jiff_instance.id;
+    var shares = jiff_instance.share(my_input, 3);
+    console.log(my_input);
 
+    var more_secure_share = shares[1].change_threshold(4);
+    console.log(more_secure_share);
 
-    for (var i = 0; i < 100; i++) {
-      // generate beaver triple
-      var id = jiff_instance.counters.gen_op_id('lt_hp', [1,2,3]);
-      try {
-        // TODO build generate_bit_sequences (mpdify to actually work) instead of using server_generate_and_share.
-        jiff_instance.preprocessing('bit_sequences', 'lt_hp', jiff_instance.server_generate_and_share({bit: true,
-          count: 8
-        }, ['1','2','3'], jiff_instance.threshold, jiff_instance.Zp, id +':number:' ), 1, []);
-      } catch (e) {
-        console.log(e);
+    return jiff_instance.open(more_secure_share);*/
+    try {
+      //  TODO build generate_bit_sequences (mpdify to actually work) instead of using server_generate_and_share.
+      for (var i = 0; i < 100; i++) {
+        jiff_instance.preprocessing('bit_sequences', 'lt_hp', jiff_instance.protocols.generate_random_bit_sequence, 100);
       }
+    } catch (e) {
+      console.log(e);
     }
 
-    jiff_instance.counters.op_count['1,2,3'] = 0;
+    jiff_instance.counters.op_count['lt_hp'] = 0;
 
-    var values = [];
-    // generate values to multiply
-    for (var j = 0; j < 100; j++) {
-      values.push(jiff_instance.share(j));
+    try {
+      jiff_instance.counters.op_count['1,2,3'] = 0;
+
+      var values = [];
+      //    generate values to multiply
+      for (var j = 0; j < 100; j++) {
+        values.push(jiff_instance.share(j));
+      }
+
+      var results = [];
+      for (var k = 1; k < values.length - 2; k++) {
+        //console.log("comparing values:", k, k+1);
+        var result = values[k][1].slt(values[k+1][1]);
+        results.push(result);
+      }
+
+      // Return a promise to the final output(s)
+      return jiff_instance.open_array(results);
+    } catch (e) {
+      console.log(e);
     }
-
-    var results = [];
-    for (var k = 0; k < values.length - 1; k++) {
-      var result = values[k].slt(values[k+1]);
-      results.push(result);
-    }
-
-    // Return a promise to the final output(s)
-    return jiff_instance.open_array(results);
   };
 }((typeof exports == 'undefined' ? this.mpc = {} : exports), typeof exports != 'undefined'));
