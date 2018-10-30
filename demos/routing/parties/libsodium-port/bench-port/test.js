8380139e288b7dc1aa44fd6faa65cc65d49c22d4
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2018-10-30 02:02:46 -0400

    Add binding for generating random scalar: started turning test into wrapper API

--- demos/routing/parties/libsodium-port/bench-port/test.js
+++ demos/routing/parties/libsodium-port/bench-port/test.js
@@ -2,62 +2,71 @@
 var native = require('../../../build/Release/obj.target/native.node');
 var BN = require('bn.js');
 
-// convert int to Uint8Array with 32 bytes
-function to32Bytes(num) {
+// init
+native.initSodium();
+
+// Byte conversions
+function bytesToBN(bytes) {
+  var result = new BN('0');
+  for (var i = bytes.length-1; i >= 0; i--) {
+    var b = new BN(bytes[i]);
+    result = result.or(b).shln(i * 8);
+  }
+  return result;
+}
+function BNToBytes(num) {
   var bytes = new Uint8Array(32);
   var str = num.toString(2);
-  
-  while(str.length < 32 * 8) {
+
+  while (str.length < 32 * 8) {
     str = '0' + str;
   }
-  
-  for(var i = 0; i < 32; i++) {
+
+  for (var i = 0; i < 32; i++) {
     var byte = '';
-    for(var j = 0; j < 8; j++) {
+    for (var j = 0; j < 8; j++) {
       byte +=  str.charAt(i*8 + j);
     }
     bytes[i] = parseInt(byte, 2);
   }
-  
+
   return bytes.reverse();
 }
 
-// Some scalar
-var order = new BN(2).pow(new BN("252")).add(new BN("27742317777372353535851937790883648493"));
-var scalar = new BN(2);
-var inv = scalar.invm(order);
-
-// Some point
+// Find and hash some Point
 var label = new Uint8Array(32);
 label[13] = 1;
-
-// init
-console.log('init', native.initSodium());
-
-// hash point
 var point = native.hashToPoint(label.buffer);
 point = point.slice(0); // copy
 point = new Uint8Array(point);
-console.log('hash', point);
 
-// start test
-// 1st mult
-var result = native.applyPRF(to32Bytes(scalar).buffer, point.buffer);
-result.slice(0);
-result = new Uint8Array(result);
-console.log('1st', result);
+// Find random scalar and its inverse
+for (var t = 0; t < 1000; t++) {
+  var order = new BN(2).pow(new BN('252')).add(new BN('27742317777372353535851937790883648493'));
+  var scalar = native.randomScalar().slice(0);
+  scalar = new Uint8Array(scalar);
+  scalar = bytesToBN(scalar).mod(order);
+  var inv = scalar.invm(order);
+  scalar = BNToBytes(scalar).buffer;
+  inv = BNToBytes(inv).buffer;
 
-// 2nd mult: by inverse
-var result = native.applyPRF(to32Bytes(inv).buffer, result.buffer);
-result.slice(0);
-result = new Uint8Array(result);
-console.log('2nd', result);
+  // start test
+  // 1st mult
+  var result = native.applyPRF(scalar, point.buffer);
+  result.slice(0);
+  result = new Uint8Array(result);
 
-var equal = true;
-for(var i = 0; i < 32; i++) {
-  if(result[i] != point[i]) {
-    equal = false;
-    break;
+  // 2nd mult: by inverse
+  result = native.applyPRF(inv, result.buffer);
+  result.slice(0);
+  result = new Uint8Array(result);
+
+  for (var i = 0; i < 32; i++) {
+    if (result[i] !== point[i]) {
+      console.log('problem');
+      return;
+    }
   }
 }
-console.log(equal);
+
+console.log('success');
