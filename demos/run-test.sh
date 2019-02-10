c34b2ed650cfd708e45f44ecdd5a1eca4be8e677
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2019-02-10 17:12:47 -0500

    Reorganize

--- demos/run-test.sh
+++ demos/run-test.sh
@@ -6,7 +6,7 @@ if [ "$1" == "*" ]; then
     EXIT_CODE=0
     for i in demos/*; do
         if [ -f "$i/test.js" ]; then
-            if ! [[ "$i" =~ ^demos/(pca|restAPI|template|table-join-filter-count|mpc-as-a-service)$ ]]; then
+            if ! [[ "$i" =~ ^demos/(pca|restAPI|template|mpc-as-a-service)$ ]]; then
                 sleep 2
                 npm run-script test-demo -- "$i"
                 CODE=$?
