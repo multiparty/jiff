3e7e7792e16763b0f4b6bee714283c3d57d9cb01
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2019-03-21 09:38:16 -0400

    Add workshop examples and fixes

---
+++ workshop/mpc-web/analyst.js
@@ -0,0 +1,77 @@
+/* Handle storing and loading keys */
+var KEYS_FILE = './keys.json';
+
+// Depdendencies
+var jiff = require('../../lib/jiff-client');
+var fs = require('fs');
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+// Initialization
+var options = { party_id: 1 };
+var keys = load_keys();
+options.public_key = keys.public_key;
+options.secret_key = keys.secret_key;
+var jiff_instance = jiff.make_jiff('http://localhost:8080', '1', options);
+
+// Wait for server to connect
+jiff_instance.wait_for(['s1'], function () {
+  save_keys();
+  console.log('When enough parties submit enter a line');
+  // begin computation on line enter
+  rl.on('line', function (_) {
+    // send begin signal
+    jiff_instance.emit('begin', [ 's1' ], '');
+
+    // receive number of parties from server
+    jiff_instance.listen('number', function (_, party_count) {
+      party_count = parseInt(party_count);
+      console.log('BEGIN: # of parties ' + party_count);
+
+      // Computation starts
+      var shares = {};
+      for (var i = 2; i <= party_count; i++) {
+        shares[i] = jiff_instance.share(null, 2, [1, 's1'], [ i ])[i];
+      }
+
+      var sum = shares[2];
+      for (var p = 3; p <= party_count; p++) {
+        sum = sum.sadd(shares[p]);
+      }
+
+      jiff_instance.open(sum, [1]).then(function (sum) {
+        console.log('SUM IS: ' + sum);
+      });
+    });
+  });
+});
+
+function save_keys() {
+  var public_key = '['+jiff_instance.public_key.toString()+']';
+  var secret_key = '['+jiff_instance.secret_key.toString()+']';
+  var obj = '{ "public_key": '+public_key+', "secret_key": '+secret_key+'}';
+  fs.writeFile(KEYS_FILE, obj, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+function load_keys() {
+  try {
+    var obj = require(KEYS_FILE);
+    obj.secret_key = new Uint8Array(obj.secret_key);
+    obj.public_key = new Uint8Array(obj.public_key);
+    return obj;
+  } catch (err) {
+    // key file does not exist
+    return { public_key: null, secret_key: null };
+  }
+}
+
+
+
---
+++ workshop/outsourced/server.js
@@ -0,0 +1,82 @@
+/**
+ * This is a server instance, it just routes communication
+ * between different parties.
+ * To run, use:
+ *  node server.js [path/to/configuration/file]
+ * Configuration file path is optional, by default ./config.js
+ * will be used.
+ */
+console.log('Command line arguments: [/path/to/configuration/file.json]');
+
+// Server setup
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// Read configuration
+var config = './config.json';
+if (process.argv[2] != null) {
+  config = process.argv[2];
+}
+config = require(config);
+
+// Keep track of assigned ids
+var assignedCompute = {};
+var assignedInput = {};
+var options = {
+  logs: true,
+  hooks: {
+    beforeInitialization: [
+      function (jiff, computation_id, msg, params) {
+        console.log(msg, params);
+        if (params.party_id != null) {
+          return params;
+        }
+
+        var search = config.compute_parties;
+        var check = assignedCompute;
+        if (msg.role === 'input') {
+          search = config.input_parties;
+          check = assignedInput;
+        }
+
+        for (var p = 0; p < search.length; p++) {
+          var id = search[p];
+          if (check[id] === true) {
+            continue;
+          }
+
+          check[id] = true;
+          params.party_id = id;
+          console.log(id);
+          return params;
+        }
+
+        return params;
+      }
+    ]
+  }
+};
+
+// Create the server
+require('../../lib/jiff-server').make_jiff(http, options);
+
+// Serve static files.
+app.get('/config.js', function (req, res) {
+  var str = 'var config = \'' + JSON.stringify(config) + '\';\n';
+  str += 'config = JSON.parse(config);';
+  res.send(str);
+});
+
+app.use('/workshop', express.static('workshop'));
+app.use('/lib', express.static('lib'));
+app.use('/lib/ext', express.static('lib/ext'));
+http.listen(8080, function () {
+  console.log('listening on *:8080');
+});
+
+console.log('** To provide inputs, direct your browser to *:8080/demos/mpc-as-a-service/client.html.');
+console.log('** To run a compute party, use the command line and run node compute-party.js [configuration-file] [computation-id]');
+console.log('All compute parties must be running before input parties can connect, an input party can leave');
+console.log('any time after it submits its input.');
+console.log('');
