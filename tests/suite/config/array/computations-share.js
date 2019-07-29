b94153ca7c7ba51be1824971973c63e69ef2ea86
p-flock <pflock@bu.edu>
2019-07-29 11:55:14 -0400

    Added test suite for arrays, only tests sharing

---
+++ tests/suite/config/array/computations-share.js
@@ -0,0 +1,84 @@
+var baseComputations = require('../../computations.js');
+
+baseComputations.mpcInterpreter = 'MPC';
+baseComputations.openInterpreter = 'OPEN';
+
+baseComputations.shareParameters = function (jiff_instance, test, testInputs) {
+  var numbers = testInputs['numbers'];
+
+  var input = numbers[jiff_instance.id]; // will be ignored if party is not a sender
+  var threshold = testInputs['threshold'];
+  var receivers = testInputs['receivers'];
+  var senders = testInputs['senders'];
+
+  return {
+    input: input,
+    threshold: threshold,
+    receivers: receivers,
+    senders: senders
+  }
+};
+
+baseComputations.shareHook = function (jiff_instance, test, testInputs, input, threshold, receivers, senders) {
+  var id = jiff_instance.id;
+  if (receivers.indexOf(id) === -1 && senders.indexOf(id) === -1) {
+    return null;
+  }
+
+  console.log(input);
+  return jiff_instance.share_ND_array(input, null,threshold, receivers, senders);
+};
+
+baseComputations.singleCompute = function (jiff_instance, shareParameters, test, values, interpreter) {
+  if (interpreter === baseComputations.openInterpreter) {
+    return values['numbers'][jiff_instance.id];
+  }
+
+  // MPC interpreter
+  // If we got here, we must be either a sender or a receiver
+
+  // Case 1: did not receive any shares, must have sent one, will receive an open.
+  if (shareParameters.receivers.indexOf(jiff_instance.id) === -1) {
+    console.log('did not receive any shares:', shareParameters)
+    return jiff_instance.receive_open(shareParameters.receivers, shareParameters.threshold);
+  }
+
+  // Case 2: received shares, maybe sent one too
+  var promise = null; // if sent a share, this will be assigned a promise to it when it is opened
+  for (var i = 0; i < shareParameters.senders.length; i++) {
+    // Loop over received shares, open each one to its original sender
+    var pid = shareParameters.senders[i];
+    var shares = values[pid];
+    console.log(shares == null);
+    var tmp = jiff_instance.open_ND_array(shares, [pid]);
+    if (tmp != null) { // pid == jiff_instance.id
+      promise = tmp;
+    }
+  }
+
+  return promise;
+};
+
+baseComputations.openHook = async function (jiff_instance, test, promise) {
+  return await promise;
+};
+
+baseComputations.errorMessage = function (jiff_instance, test, testInputs, shareParameters, mpcResult, expectedResult) {
+  var msg = 'party id '+ jiff_instance.id +': ';
+  msg += 'senders: [ ' + shareParameters.senders.join(', ') + ' ]. ';
+  msg += 'receivers: [ ' + shareParameters.receivers.join(', ') + ' ]. ';
+  msg += '!= ' + mpcResult.toString() + ' ----- Expected ' + expectedResult.toString() + '. ';
+  msg += 'All Inputs: [ ' + baseComputations.myJoin(shareParameters.senders, testInputs['numbers'], ' | ') + ' ]';
+  return msg;
+};
+
+baseComputations.successMessage = function (jiff_instance, test, testInputs, shareParameters, mpcResult, expectedResult) {
+  var msg = 'party id '+ jiff_instance.id +': ';
+  msg += 'senders: [ ' + shareParameters.senders.join(', ') + ' ]. ';
+  msg += 'receivers: [ ' + shareParameters.receivers.join(', ') + ' ]. ';
+  msg += '= ' + mpcResult.toString() + ' = ' + expectedResult.toString() + '. ';
+  msg += 'All Inputs: [ ' + baseComputations.myJoin(shareParameters.senders, testInputs['numbers'], ' | ') + ' ]';
+  return msg;
+};
+
+module.exports = baseComputations;
---
+++ tests/suite/config/array/generation.js
@@ -0,0 +1,194 @@
+exports.generateUniform = function (test, options, max) {
+  return Math.floor(Math.random() * max);
+};
+exports.generateNonZeroUniform = function (test, options, max) {
+  return Math.floor(Math.random() * (max - 1)) + 1;
+};
+exports.generateBit = function (/*test, options*/) {
+  return Math.random() < 0.5 ? 0 : 1;
+};
+exports.generateMultiple = function (test, options, max, factor) {
+  var coef = exports.generateUniform(test, options, Math.floor(max / factor));
+  return coef * factor;
+};
+exports.generateDividend = function (test, options, max, divisor) {
+  return exports.generateUniform(test, options, max);
+};
+
+// Generation API referred to from configuration JSON files
+
+// Sharing/Opening test cases with no operations
+exports.generateShareInputs = function (test, count, options) {
+  var all_parties = [];
+  for (var k = 1; k <= options.party_count; k++) {
+    all_parties.push(k);
+  }
+
+  var inputs = [];
+  // Generate test cases one at a time
+  // A test case consists of
+  // 1) input numbers
+  // 2) sharing threshold
+  // 3) array of senders
+  // 4) array of receivers
+  var max = options.max || options.Zp;
+  for (var t = 0; t < count; t++) {
+    var oneTest = { numbers: {} };
+    // Generate numbers
+    for (var p = 1; p <= options.party_count; p++) {
+      var arr = [];
+      for (var x = 0; x < 4; x++) {
+        arr.push(exports.generateUniform(test, options, max));
+      }
+      oneTest['numbers'][p] = arr;
+    }
+    // 1 <= Threshold <= party_count
+    oneTest['threshold'] = Math.floor(Math.random() * options.party_count) + 1;
+
+    // Generate senders/receivers
+    var sn = Math.ceil(Math.random() * options.party_count); // At least one sender, at most all.
+    var rn = oneTest['threshold'] + Math.floor(Math.random() * (options.party_count - oneTest['threshold'] + 1)); // At least as many receivers as threshold.
+
+    // Generate actual receivers and senders arrays
+    var senders = all_parties.slice();
+    var receivers = all_parties.slice();
+
+    // remove random parties until proper counts are reached.
+    while (senders.length > sn) {
+      senders.splice(Math.floor(Math.random() * senders.length), 1);
+    }
+    while (receivers.length > rn) {
+      receivers.splice(Math.floor(Math.random() * receivers.length), 1);
+    }
+    oneTest['senders'] = senders;
+    oneTest['receivers'] = receivers;
+
+    inputs.push(oneTest);
+  }
+  return inputs;
+};
+
+// Arithmetic inputs: one for each party except for / and % where only two inputs are needed.
+// | and ^ requires bits, while thr denominator for / and % must be non-zero.
+exports.generateArithmeticInputs = function (test, count, options) {
+  var party_count = options.party_count;
+  var max = options.max || options.Zp;
+
+  var inputs = [];
+  var t, p, oneInput;
+  if (test.startsWith('/') || test === '%') {
+    // division and mod: only two inputs, the second is non-zero.
+    for (t = 0; t < count; t++) {
+      oneInput = {};
+      oneInput[2] = exports.generateNonZeroUniform(test, options, max);
+      oneInput[1] = exports.generateDividend(test, options, max, oneInput[2]);
+      inputs.push(oneInput);
+    }
+  } else if (test === '|' || test === '^') {
+    // or and xor: inputs are binary
+    for (t = 0; t < count; t++) {
+      oneInput = {};
+      for (p = 1; p <= party_count; p++) {
+        oneInput[p] = exports.generateBit(test, options);
+      }
+      inputs.push(oneInput);
+    }
+  } else if (test === '!') {
+    for (t = 0; t < count; t++) {
+      oneInput = {};
+      oneInput[1] = exports.generateBit(test, options);
+      inputs.push(oneInput);
+    }
+    return inputs;
+  } else {
+    // otherwise no constraints
+    for (t = 0; t < count; t++) {
+      oneInput = {};
+      for (p = 1; p <= party_count; p++) {
+        oneInput[p] = exports.generateUniform(test, options, max);
+      }
+      inputs.push(oneInput);
+    }
+  }
+
+  return inputs;
+};
+
+// Constant Arithmetic inputs: an input for the first party, and a constant input.
+// | and ^ requires bits, while thr denominator for / and % must be non-zero.
+exports.generateConstantArithmeticInputs = function (test, count, options) {
+  var max = options.max || options.Zp;
+  var cmax = options.cmax || max;
+
+  var inputs = [];
+  var t, oneInput;
+  if (test.indexOf('/') > -1 || test.indexOf('%') > -1 || test === 'cdivfac') {
+    // division and mod: only two inputs, the second is non-zero.
+    for (t = 0; t < count; t++) {
+      oneInput = {};
+      oneInput['constant'] = exports.generateNonZeroUniform(test, options, cmax);
+      if (test === 'cdivfac') {
+        oneInput[1] = exports.generateMultiple(test, options, max, oneInput['constant']);
+      } else {
+        oneInput[1] = exports.generateDividend(test, options, max, oneInput['constant']);
+      }
+
+      inputs.push(oneInput);
+    }
+  } else if (test === '|' || test === '^') {
+    // or and xor: inputs are binary
+    for (t = 0; t < count; t++) {
+      oneInput = {};
+      oneInput[1] = exports.generateBit(test, options);
+      oneInput['constant'] = exports.generateBit(test, options);
+      inputs.push(oneInput);
+    }
+  } else {
+    // otherwise no constraints
+    for (t = 0; t < count; t++) {
+      oneInput = {};
+      oneInput[1] = exports.generateUniform(test, options, max);
+      oneInput['constant'] = exports.generateUniform(test, options, cmax);
+      inputs.push(oneInput);
+    }
+  }
+
+  return inputs;
+};
+
+// Constant Comparison inputs: two inputs for the first two parties only
+// if using random numbers and large Zp, inputs will be very unlikely to be equal
+// we must make it likely for the inputs to be equal manually for == and != checks
+exports.generateComparisonInputs = function (test, count, options) {
+  var max = options.max || options.Zp;
+
+  var inputs = [];
+  // make it likely that the numbers are equal.
+  for (var t = 0; t < count; t++) {
+    var oneInput = {};
+    oneInput[1] = exports.generateUniform(test, options, max);
+    oneInput[2] = Math.random() < 0.5 ? oneInput[1] : exports.generateUniform(test, options, max);
+    inputs.push(oneInput);
+  }
+
+  return inputs;
+};
+
+// Constant Comparison inputs: an input for the first party, and a constant input.
+// if using random numbers and large Zp, inputs will be very unlikely to be equal
+// we must make it likely for the inputs to be equal manually for == and != checks
+exports.generateConstantComparisonInputs = function (test, count, options) {
+  var max = options.max || options.Zp;
+  var cmax = options.cmax || max;
+
+  var inputs = [];
+  // make it likely that the numbers are equal.
+  for (var t = 0; t < count; t++) {
+    var oneInput = {};
+    oneInput[1] = exports.generateUniform(test, options, max);
+    oneInput['constant'] = Math.random() < 0.5 ? oneInput[1] : exports.generateUniform(test, options, cmax);
+    inputs.push(oneInput);
+  }
+
+  return inputs;
+};
