7e111c746de15c9325cdfe440539dfe96000f3e4
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2019-09-25 02:33:45 -0400

    Binary search tutorial

--- tutorials/index.js
+++ tutorials/index.js
@@ -8,12 +8,11 @@ const plotly = ['static/js/plotly-latest.min.js'];
 const neptune = new Neptune();
 neptune.addDocument('MPC', '0-intro-to-mpc.md', true, jsDependencies.concat(plotly));
 neptune.addDocument('intro', '1-intro.md', true, jsDependencies);
-neptune.addDocument('inner-product', '2-innerprod.md', true, jsDependencies);
-neptune.addDocument('standard-deviation', '3-standarddev.md', true, jsDependencies);
-neptune.addDocument('preprocessing', '4-preprocessing.md', true, jsDependencies);
-neptune.addDocument('binary-search', '5-binarysearch.md', true, jsDependencies);
-neptune.addDocument('voting', '6-voting.md', true, jsDependencies);
-neptune.addDocument('ifelse', '7-ifelse.md', true, jsDependencies);
+neptune.addDocument('voting', '2-voting.md', true, jsDependencies);
+neptune.addDocument('binary-search', '3-binarysearch.md', true, jsDependencies);
+neptune.addDocument('standard-deviation', '4-standarddev.md', true, jsDependencies);
+neptune.addDocument('inner-product', '5-innerprod.md', true, jsDependencies);
+neptune.addDocument('preprocessing', '6-preprocessing.md', true, jsDependencies);
 
 neptune.start(9111);
 
