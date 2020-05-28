56e01aabc7e838db080475a95a973c582ca002e4
Wyatt Howe <wyatt@wyatthowe.net>
2020-05-28 11:18:33 -0400

    update array demos for new sharing api
    (share array returns actual shares now)

--- demos/array-merge-sort/mpc.js
+++ demos/array-merge-sort/mpc.js
@@ -80,28 +80,22 @@
     var final_promise = final_deferred.promise();
 
     // Share the arrays
-    jiff_instance.share_array(input, input.length).then(function (shares) {
-      jiff_instance.seed_ids(seed);
+    var shares = jiff_instance.share_array(input, input.length);
+    jiff_instance.seed_ids(seed);
 
-      // sum all shared input arrays element wise
-      var array = shares[1];
-      for (var p = 2; p <= jiff_instance.party_count; p++) {
-        for (var i = 0; i < array.length; i++) {
-          array[i] = array[i].sadd(shares[p][i]);
-        }
+    // Sum all shared input arrays element wise
+    var array = shares[1];
+    for (var p = 2; p <= jiff_instance.party_count; p++) {
+      for (var i = 0; i < array.length; i++) {
+        array[i] = array[i].sadd(shares[p][i]);
       }
-      // sort new array
-      oddEvenSort(array, 0, array.length);
+    }
+    // Sort new array
+    oddEvenSort(array, 0, array.length);
 
-      // Open the array
-      var allPromises = [];
-      for (var k = 0; k < array.length; k++) {
-        allPromises.push(jiff_instance.open(array[k]));
-      }
-
-      Promise.all(allPromises).then(function (results) {
-        final_deferred.resolve(results);
-      });
+    // Open the array
+    jiff_instance.open_ND_array(array).then(function (results) {
+      final_deferred.resolve(results);
     });
 
     return final_promise;
