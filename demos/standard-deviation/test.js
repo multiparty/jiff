2fc9231b45a497196c9d636964810e8a2f23d705
Kinan Bab <11751905+KinanBab@users.noreply.github.com>
2020-11-12 14:07:41 -0500

    Fixes and improvements in standard deviation demo: (#214)
    * Use negative number extension so that we can handle minor fixed point errors that cause quantities close to zero to be negative (but also close to zero)
    * Improve protocol to make it more efficient and also much clearer to where fixed point errors are introduced
    * Simplify testing logic, add test cases where all inputs are the same and the output must be exactly 0
    * Minor fix in fixedpoint extension to ensure cmult and smult have the same semantic behavior when div=false and negative number extension is also used

--- demos/standard-deviation/test.js
+++ demos/standard-deviation/test.js
@@ -6,12 +6,12 @@ var showProgress = true;
 
 // Generic Testing Parameters
 var party_count = 3;
-var parallelismDegree =5; // Max number of test cases running in parallel
+var parallelismDegree = 5; // Max number of test cases running in parallel
 var n = 20; // Number of test cases in total
 
 // Parameters specific to this demo
 var magnitude = 3; // 3 digits of magnitude
-var accuracy = 2; // 2 digits of accuracy after decimal point
+var accuracy = 3; // 3 digits of accuracy after decimal point
 var Zp = new BigNumber(32416190071);
 var maxValue = 10;
 
@@ -19,24 +19,16 @@ var maxValue = 10;
   Truncate any decimal points beyond accuracy. (Can't use built in bignumber functions since none of built-in rounding
  modes are correct.)
  */
-function bigNumAccuracy(num, accuracy) {
+function truncate(num) {
   var numStr = num.toString();
   var numParts = numStr.split('.');
+  var truncdNum;
   if (numParts.length > 1) {
-    var truncdNum = numParts[0] + '.' + numParts[1].substring(0,accuracy);
+    truncdNum = numParts[0] + '.' + numParts[1].substring(0, accuracy);
   } else {
     truncdNum = numParts[0]
   }
   return new BigNumber(truncdNum);
-}
-
-/**
- * Round any decimal points to accuracy.
-* */
-function bigNumRound(num, accuracy) {
-  var numFloat = parseFloat(num.toString());
-  var numRounded = numFloat.toFixed(accuracy);
-  return new BigNumber(numRounded);
 }
 
 /**
@@ -48,19 +40,20 @@ function bigNumRound(num, accuracy) {
  */
 function generateInputs(party_count) {
   var inputs = {};
+  var identical = Math.random() * maxValue * 2 - maxValue;
+  identical = new BigNumber(identical.toFixed(accuracy));
   for (var p = 1; p <= party_count; p++) {
-    inputs[p] = [];
+    inputs[p] = [identical];
   }
 
-  //Generate test cases one at a time
-  for (var t = 0; t < n; t++) {
+  // Generate test cases one at a time.
+  for (var t = 0; t < n - 1; t++) {
     for (var i = 1; i <= party_count; i++) {
-      var numString = (Math.random() * maxValue).toFixed(accuracy);
+      var numString = (Math.random() * 2 * maxValue - maxValue).toFixed(accuracy);
       inputs[i].push(new BigNumber(numString));
     }
   }
   return inputs;
-
 }
 
 /**
@@ -70,27 +63,30 @@ function generateInputs(party_count) {
  *   [ 'test1_output', 'test2_output', ... ]
  */
 function computeResults(inputs) {
-
   var results = [];
-
   for (var j = 0; j < n; j++) {
-    var in_sum = 0;
-    var in_squared_sum = 0;
-    for (var i = 1; i<= party_count; i++) {
-      var in_squared = bigNumRound(inputs[i][j].toPower(2), 2);
-      in_sum = inputs[i][j].plus(in_sum);
-      in_squared_sum = in_squared.plus(in_squared_sum);
+    var avg = new BigNumber(0);
+    var sq_avg = new BigNumber(0);
+
+    // Compute square of average and average of squares.
+    for (var i = 1; i <= party_count; i++) {
+      var input = inputs[i][j];
+      var sq_input = truncate(input.times(input));
+      avg = avg.plus(input);
+      sq_avg = sq_avg.plus(sq_input);
     }
 
-    var in_sum_squared = bigNumAccuracy(in_sum.toPower(2), accuracy);
-    var one_over_party = new BigNumber(Number.parseFloat((1/party_count).toFixed(accuracy)));
-    var intermediary = bigNumAccuracy(in_sum_squared.times(one_over_party), accuracy);     // intermediary = in_sum^2/n
-    intermediary = in_squared_sum.minus(intermediary);      // intermediary = in_squared_sum - in_sum^2/n
-    var variance = intermediary.dividedBy(party_count - 1);
-    results.push(variance.sqrt())
+    // Square of average.
+    avg = avg.times(avg);
+    sq_avg = sq_avg.times(party_count);
 
-
+    var diff = sq_avg.minus(avg);
+    diff = truncate(diff);
+    diff = truncate(diff.div(Math.pow(party_count, 2)));
+    results.push(truncate(diff.sqrt()));
   }
+
+  assert.deepEqual(results[0].toString(10), "0");
   return results;
 }
 
@@ -149,8 +145,8 @@ describe('Test', function () {
           // note this is different than template because of bignumbers framework and needing precision only up to certain
           // number of decimal points
           try {
-            var test = bigNumAccuracy(testResults[i], accuracy).toString();
-            var real = bigNumAccuracy(realResults[i], accuracy).toString();
+            var test = testResults[i].toString();
+            var real = realResults[i].toString();
             assert.deepEqual(test, real, msg);
           } catch (assertionError) {
             done(assertionError);
