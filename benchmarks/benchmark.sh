4985330cc92145506f84e104bcb8197497f9a7a0
p-flock <pflock@bu.edu>
2020-11-18 14:27:54 -0500

    Add all benchmark scripts to bash script and output to appropriate log files

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -7,3 +7,15 @@ node party.js 500 stdev stdevtest500 >> logtimes/stdev.log
 node party.js 10 sum sumtest10 >> logtimes/sum.log
 node party.js 100 sum sumtest100 >> logtimes/sum.log
 node party.js 500 sum sumtest500 >> logtimes/sum.log
+
+node party.js 10 avg avgtest10 >> logtimes/avg.log
+node party.js 100 avg avgtest100 >> logtimes/avg.log
+node party.js 500 avg avgtest500 >> logtimes/avg.log
+
+node party.js 10 min mintest10 >> logtimes/min.log
+node party.js 100 min mintest100 >> logtimes/min.log
+node party.js 500 min mintest500 >> logtimes/min.log
+
+node party.js 10 max maxtest10 >> logtimes/max.log
+node party.js 100 max maxtest100 >> logtimes/max.log
+node party.js 500 min maxtest500 >> logtimes/max.log
