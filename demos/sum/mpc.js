92aa5e15a97a4a5bb78ae45b9490ae067dd627f6
p-flock <pflock@bu.edu>
2021-03-17 16:23:52 -0400

    WIP websockets distribution for browser

--- demos/sum/mpc.js
+++ demos/sum/mpc.js
@@ -12,14 +12,14 @@
       // eslint-disable-next-line no-undef
       JIFFClient = require('../../lib/jiff-client');
       // eslint-disable-next-line no-undef
-      jiff_websockets = require('../../lib/ext/jiff-client-websockets.js');
+      JIFFClient.jiff_websockets = require('../../lib/ext/jiff-client-websockets.js');
     }
 
     opt.autoConnect = false;
     // eslint-disable-next-line no-undef
     saved_instance = new JIFFClient(hostname, computation_id, opt);
     // eslint-disable-next-line no-undef
-    saved_instance.apply_extension(jiff_websockets, opt);
+    saved_instance.apply_extension(JIFFClient.jiff_websockets, opt);
     saved_instance.connect();
 
     return saved_instance;
