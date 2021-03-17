5492ec87f8bf6a76e0fb09496bcc0868c1199c5d
tsbraun1891 <tsbraun1891@gmail.com>
2021-03-17 15:57:36 -0400

    Fix load websockets file bug

--- demos/sum/server.js
+++ demos/sum/server.js
@@ -8,9 +8,13 @@ var http = require('http').Server(app);
 app.use('/demos', express.static(path.join(__dirname, '..', '..', 'demos')));
 app.use('/dist', express.static(path.join(__dirname, '..', '..', 'dist')));
 app.use('/lib/ext', express.static(path.join(__dirname, '..', '..', 'lib', 'ext')));
+app.use('/lib/common/linkedlist.js', express.static(path.join(__dirname, '..', '..', 'lib', 'common', 'linkedlist.js')));
+
+console.log(path.join(__dirname, '..', '..', 'lib', 'common', 'linkedlist.js'))
 
 var JIFFServer = require('../../lib/jiff-server.js');
 var jiff_instance = new JIFFServer(http, { logs: true });
+
 
 var jiffWebsocketServer = require('../../lib/ext/jiff-server-websockets');
 jiff_instance.apply_extension(jiffWebsocketServer);
