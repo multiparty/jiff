0f7f26b69908353b959fcb24e18ac128b2833a14
Harunobu Ishii <harunobuishii547@gmail.com>
2024-04-01 16:34:36 -0400

    vote completed

---
+++ tests/regr-tests/voting.test.ts
@@ -0,0 +1,104 @@
+describe('JIFF Voting Operations', () => {
+  var jiffClients: any[] = [];
+  var jiffServer: any;
+  var server: any;
+  var entries: { [key: number]: number[] } = { 1: [1, 0, 0, 0], 2: [0, 0, 0, 1], 3: [0, 0, 0, 1] };
+  var computation_id = 'test-voting';
+  const party_count = 3;
+
+  beforeEach(async () => {
+    // Server Setup
+    let port: number = 8112;
+    const init_server = require('./server');
+    const servers = init_server(port);
+    (jiffServer = servers[0]), (server = servers[1]);
+    await new Promise((resolve) => server.on('listening', resolve)); // Wait for server to be ready
+
+    // Client Setup
+    const JIFFClient = require('../../lib/jiff-client.js');
+    const baseUrl = `http://localhost:${port}`;
+    const options = {
+      party_count: party_count,
+      crypto_provider: true
+    };
+
+    jiffClients = Array.from({ length: party_count }, () => new JIFFClient(baseUrl, computation_id, options));
+
+    const jiff_negativenumber = require('../../lib/ext/jiff-client-negativenumber.js');
+
+    function apply_extension(jiff: any) {
+      jiff.apply_extension(jiff_negativenumber, options);
+    }
+    jiffClients.map((client, _) => apply_extension(client));
+  });
+
+  afterEach(async () => {
+    // Shutting down client
+    jiffClients.map(async (client, _) => await client.socket.disconnect());
+
+    // Shutting down Server
+    await jiffServer.closeAllSockets();
+  });
+
+  it('should correctly find the majority vote', async () => {
+    async function sanityCheck(shares: any) {
+      // first check: if sum of values in an array/share = 1
+      var sum = shares[0];
+      for (var i = 1; i < shares.length; i++) {
+        sum = await sum.sadd(shares[i]);
+      }
+      var check1 = await sum.ceq(1);
+
+      // second check: if all elements are <= 1
+      var check2 = await shares[0].clteq(1);
+      for (var j = 1; j < shares.length; j++) {
+        check2 = await check2.smult(shares[j].clteq(1));
+      }
+
+      // 1(=true) only if both first & second checks pass
+      const sanity_flag = await check1.smult(check2);
+      return sanity_flag;
+    }
+
+    async function find_most_popular(jiffClient: any, id: number) {
+      return new Promise((resolve, reject) => {
+        jiffClient.wait_for([1, 2, 3], async () => {
+          try {
+            const input = await jiffClient.share_array(entries[id]);
+            const checker1 = await sanityCheck(input[1]);
+            const checker2 = await sanityCheck(input[2]);
+            const checker3 = await sanityCheck(input[3]);
+            var sanity_flag = await checker1.sadd(checker2);
+            sanity_flag = await sanity_flag.sadd(checker3);
+            sanity_flag = await sanity_flag.ceq(3);
+
+            // Aggregating all votes into the array named 'vote'
+            var vote = input[1];
+            for (let party = 2; party <= jiffClient.party_count; party++) {
+              for (let idx = 0; idx < vote.lenth; idx++) {
+                vote[idx] = await vote[idx].sadd(input[party][idx]);
+              }
+            }
+
+            // Check who's got the majority vote
+            var majo_idx = 0;
+            var curr_max = vote[0];
+            for (let i = 1; i < vote.length; i++) {
+              var iIsMax = await vote[i].sgt(curr_max);
+              majo_idx = await iIsMax.if_else(i, majo_idx);
+            }
+            majo_idx = await sanity_flag.if_else(majo_idx, -1);
+
+            const result = await jiffClient.open(majo_idx);
+            resolve(result.toString(10));
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+    }
+
+    const results = await Promise.all(jiffClients.map((client, idx) => find_most_popular(client, idx + 1)));
+    results.map((res) => expect(res).toEqual('3'));
+  }, 15000);
+});
