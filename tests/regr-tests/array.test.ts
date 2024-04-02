3471297f4b6996db147bea613a74fa144fe879a1
Harunobu Ishii <harunobuishii547@gmail.com>
2024-04-02 14:45:02 -0400

    uncommented some test cases in array

--- tests/regr-tests/array.test.ts
+++ tests/regr-tests/array.test.ts
@@ -60,71 +60,71 @@ describe('JIFF Array Operations', () => {
     results.map((res) => expect(res).toEqual([5,5,5,5]));
   });
 
-  // it('should check that 4 exists in the input array, using linear search', async () => {
-  //   async function linear_search(jiffClient: any, id: number) {
-  //     return new Promise((resolve, reject) => {
-  //       jiffClient.wait_for([1, 2], async () => {
-  //         try {
-  //           const inputs = id == 1 ? await jiffClient.share_array(entries) : await jiffClient.share_array(input);
-  //           const array = inputs[1];
-  //           const element = inputs[2];
+  it('should check that 4 exists in the input array, using linear search', async () => {
+    async function linear_search(jiffClient: any, id: number) {
+      return new Promise((resolve, reject) => {
+        jiffClient.wait_for([1, 2], async () => {
+          try {
+            const inputs = id == 1 ? await jiffClient.share_array(entries) : await jiffClient.share_array(input);
+            const array = inputs[1];
+            const element = inputs[2];
 
-  //           var occurrences = await array[0].eq(element); // check equality for the first element
-  //           for (var i = 1; i < array.length; i++) {
-  //             occurrences = await occurrences.add(array[i].eq(element));
-  //           }
-  //           const result = await jiffClient.open(occurrences.gteq(1)); // check number of occurrences >= 1
-  //           resolve(result.toString(10));
-  //         } catch (error) {
-  //           reject(error);
-  //         }
-  //       });
-  //     });
-  //   }
+            var occurrences = await array[0].eq(element); // check equality for the first element
+            for (var i = 1; i < array.length; i++) {
+              occurrences = await occurrences.add(array[i].eq(element));
+            }
+            const result = await jiffClient.open(occurrences.gteq(1)); // check number of occurrences >= 1
+            resolve(result.toString(10));
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+    }
 
-  //   const results = await Promise.all(jiffClients.map((client, idx) => linear_search(client, idx + 1)));
-  //   results.map((res) => expect(res).toEqual('1'));
-  // });
+    const results = await Promise.all(jiffClients.map((client, idx) => linear_search(client, idx + 1)));
+    results.map((res) => expect(res).toEqual('1'));
+  });
 
-  // it('should check that 4 exists in the input array, using binary search', async () => {
-  //   async function binary_search(jiffClient: any, id: number) {
-  //     return new Promise((resolve, reject) => {
-  //       jiffClient.wait_for([1, 2], async () => {
-  //         try {
-  //           const inputs = id == 1 ? await jiffClient.share_array(entries) : await jiffClient.share_array(input);
-  //           const array = inputs[1];
-  //           const element = inputs[2];
+  it('should check that 4 exists in the input array, using binary search', async () => {
+    async function binary_search(jiffClient: any, id: number) {
+      return new Promise((resolve, reject) => {
+        jiffClient.wait_for([1, 2], async () => {
+          try {
+            const inputs = id == 1 ? await jiffClient.share_array(entries) : await jiffClient.share_array(input);
+            const array = inputs[1];
+            const element = inputs[2];
 
-  //           async function _bin_search(array: any[], element: any) {
-  //             if (array.length == 1) {
-  //               return await array[0].eq(element);
-  //             }
+            async function _bin_search(array: any[], element: any) {
+              if (array.length == 1) {
+                return await array[0].eq(element);
+              }
 
-  //             const mid = Math.floor(array.length / 2);
-  //             const cmp = await element.lt(array[mid]);
-  //             var nArray = [];
-  //             for (var i = 0; i < mid; i++) {
-  //               var c1 = array[i];
-  //               var c2 = array[mid + i];
-  //               nArray[i] = await cmp.if_else(c1, c2);
-  //             }
-  //             if (2 * mid < array.length) {
-  //               nArray[mid] = array[2 * mid];
-  //             }
-  //             return await _bin_search(nArray, element);
-  //           }
+              const mid = Math.floor(array.length / 2);
+              const cmp = await element.lt(array[mid]);
+              var nArray = [];
+              for (var i = 0; i < mid; i++) {
+                var c1 = array[i];
+                var c2 = array[mid + i];
+                nArray[i] = await cmp.if_else(c1, c2);
+              }
+              if (2 * mid < array.length) {
+                nArray[mid] = array[2 * mid];
+              }
+              return await _bin_search(nArray, element);
+            }
 
-  //           const occurrences = await _bin_search(array, element);
-  //           const result = await jiffClient.open(occurrences.gteq(1)); // check number of occurrences >= 1
-  //           resolve(result.toString(10));
-  //         } catch (error) {
-  //           reject(error);
-  //         }
-  //       });
-  //     });
-  //   }
+            const occurrences = await _bin_search(array, element);
+            const result = await jiffClient.open(occurrences.gteq(1)); // check number of occurrences >= 1
+            resolve(result.toString(10));
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+    }
 
-  //   const results = await Promise.all(jiffClients.map((client, idx) => binary_search(client, idx + 1)));
-  //   results.map((res) => expect(res).toEqual('1'));
-  // });
+    const results = await Promise.all(jiffClients.map((client, idx) => binary_search(client, idx + 1)));
+    results.map((res) => expect(res).toEqual('1'));
+  });
 });
